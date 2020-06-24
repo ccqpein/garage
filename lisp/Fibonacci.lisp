eca3db812e71c17a058e76aaf1d2f0fbd477dfdb
ccQpein <ccqpein@protonmail.com>
2020-06-24 09:28:16 -0400

    meow

--- lisp/Fibonacci.lisp
+++ lisp/Fibonacci.lisp
@@ -16,3 +16,15 @@
                  (inner b (+ a b) (1- n))
                  a)))
     (inner 0 1 n)))
+
+(let ((memo (make-hash-table)))
+  (defun fibonacci-2 (n)
+    (declare (fixnum n)
+             (optimize (speed 3)))
+    (let ((x (gethash n memo)))
+      (if x
+          x
+          (setf (gethash n memo)
+                (if (< n 2)
+                    n
+                    (the fixnum (+ (fibonacci-2 (1- n)) (fibonacci-2 (- n 2))))))))))
