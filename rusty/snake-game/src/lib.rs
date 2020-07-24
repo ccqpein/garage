6bbfbbc37cd809371f660eacb8a3afa52a79db1a
ccQpein <ccqpein@protonmail.com>
2020-07-24 17:05:57 -0400

    a bit

--- rusty/snake-game/src/lib.rs
+++ rusty/snake-game/src/lib.rs
@@ -1,5 +1,22 @@
 use std::io::{repeat, stdout, Read, Stdout, Write};
 
+enum Direction {
+    Right,
+    Left,
+    Up,
+    Down,
+}
+
+struct SnakeBody {
+    d: Direction,
+}
+
+struct Frame {
+    row: usize,
+    col: usize,
+    points: Vec<Vec<Option<SnakeBody>>>,
+}
+
 pub fn init_stdout(row: usize, col: usize) -> Stdout {
     let mut std_out = stdout();
     let mut a = vec![0; col];
