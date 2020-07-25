e80d26a88a0478f1c4640ed95bb8c3a1bab3f757
ccQpein <ccqpein@protonmail.com>
2020-07-25 12:45:28 -0400

    a bit

--- rusty/snake-game/src/lib.rs
+++ rusty/snake-game/src/lib.rs
@@ -22,8 +22,20 @@ pub fn init_stdout(row: usize, col: usize) -> Stdout {
     let mut a = vec![0; col];
     repeat(b'.').read_exact(&mut a).unwrap();
     a.push(b'\n');
-    for _ in 0..row {
-        std_out.write_all(&a).unwrap();
+    for i in 0..row {
+        if i == row / 2 {
+            // baby snake
+            std_out.write_all(&a).unwrap();
+            print!("\x1b[1A\x1b[{}C◼◼\n", col / 2 - 1);
+        } else {
+            std_out.write_all(&a).unwrap();
+        }
+        std_out.flush().unwrap();
     }
     std_out
+}
+
+pub fn back_to_origin(std_out: &mut Stdout) {
+    std_out.write(b"\x1b[9A\x1b[39D2").unwrap();
+    std_out.flush().unwrap();
 }
--- rusty/snake-game/src/main.rs
+++ rusty/snake-game/src/main.rs
@@ -1,5 +1,10 @@
 use snake_game::*;
+use std::{thread, time};
 
 fn main() {
-    init_stdout(5, 5);
+    let mut a = init_stdout(10, 40);
+    back_to_origin(&mut a);
+    loop {
+        thread::sleep(time::Duration::from_secs(3));
+    }
 }
