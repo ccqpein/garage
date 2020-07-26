17517a4f1be40c4782d27f56b94c030390644a7a
ccQpein <ccqpein@protonmail.com>
2020-07-26 11:27:56 -0400

    a bit

--- rusty/snake-game/src/lib.rs
+++ rusty/snake-game/src/lib.rs
@@ -1,20 +1,66 @@
-use std::io::{repeat, stdout, Read, Stdout, Write};
+use std::fmt::Arguments;
+use std::io::{repeat, stdout, Error, ErrorKind, Read, Result, Stdout, Write};
+// enum Direction {
+//     Right,
+//     Left,
+//     Up,
+//     Down,
+// }
 
-enum Direction {
-    Right,
-    Left,
-    Up,
-    Down,
-}
+// struct SnakeBody {
+//     d: Direction,
+// }
 
-struct SnakeBody {
-    d: Direction,
-}
-
-struct Frame {
+pub struct Frame {
     row: usize,
     col: usize,
-    points: Vec<Vec<Option<SnakeBody>>>,
+    points: Stdout,
+}
+
+impl Frame {
+    pub fn new(row: usize, col: usize) -> Self {
+        let mut std_out = stdout();
+        let mut a = vec![0; col];
+        repeat(b'.').read_exact(&mut a).unwrap();
+        a.push(b'\n');
+        for i in 0..row {
+            if i == row / 2 {
+                // baby snake
+                std_out.write_all(&a).unwrap();
+                print!("\x1b[1A\x1b[{}C◼◼\n", col / 2 - 1);
+            } else {
+                std_out.write_all(&a).unwrap();
+            }
+            std_out.flush().unwrap();
+        }
+
+        Self {
+            row,
+            col,
+            points: std_out,
+        }
+    }
+
+    pub fn write(&mut self, row: usize, col: usize, s: String) -> Result<()> {
+        if row >= self.row || col >= self.col {
+            return Err(Error::new(ErrorKind::Other, "beyond boundary"));
+        }
+        //let format_str = self.move_to(row, col);
+        if col == 0 {
+            write!(
+                self.points,
+                "{}",
+                format_args!("\x1b[s\x1b[{}A\r{}\x1b[u", self.row - row, s)
+            )
+        } else {
+            write!(
+                self.points,
+                "{}",
+                format_args!("\x1b[s\x1b[{}A\r\x1b[{}C{}\x1b[u", self.row - row, col, s)
+            )
+        };
+        self.points.flush()
+    }
 }
 
 pub fn init_stdout(row: usize, col: usize) -> Stdout {
@@ -36,6 +82,7 @@ pub fn init_stdout(row: usize, col: usize) -> Stdout {
 }
 
 pub fn back_to_origin(std_out: &mut Stdout) {
-    std_out.write(b"\x1b[9A\x1b[39D2").unwrap();
+    std_out.write(b"\x1b[s\x1b[9A\x1b[39D2\x1b[u").unwrap();
+    //std_out.write(b"\x1b[9B\x1b[39C").unwrap();
     std_out.flush().unwrap();
 }
--- rusty/snake-game/src/main.rs
+++ rusty/snake-game/src/main.rs
@@ -2,9 +2,15 @@ use snake_game::*;
 use std::{thread, time};
 
 fn main() {
-    let mut a = init_stdout(10, 40);
-    back_to_origin(&mut a);
-    loop {
-        thread::sleep(time::Duration::from_secs(3));
+    let mut a = Frame::new(10, 20);
+    //let mut col = 0;
+    //a.write(0, 0, String::from("◼")).unwrap();
+    //a.write(0, 1, String::from("◼")).unwrap();
+    //a.write(0, 2, String::from("◼")).unwrap();
+    for col in 0..11 {
+        a.write(col, col, String::from("◼")).unwrap();
+        thread::sleep(time::Duration::from_secs(1));
     }
+
+    a.write(0, 0, String::from("a")).unwrap();
 }
