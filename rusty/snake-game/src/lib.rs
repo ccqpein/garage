9ef850e1f8fd01cfa7ace88566b19220e17414f7
ccQpein <ccqpein@protonmail.com>
2020-07-27 10:06:47 -0400

    give snake

--- rusty/snake-game/src/lib.rs
+++ rusty/snake-game/src/lib.rs
@@ -1,15 +1,4 @@
-use std::fmt::Arguments;
 use std::io::{repeat, stdout, Error, ErrorKind, Read, Result, Stdout, Write};
-// enum Direction {
-//     Right,
-//     Left,
-//     Up,
-//     Down,
-// }
-
-// struct SnakeBody {
-//     d: Direction,
-// }
 
 pub struct Frame {
     row: usize,
@@ -23,14 +12,8 @@ impl Frame {
         let mut a = vec![0; col];
         repeat(b'.').read_exact(&mut a).unwrap();
         a.push(b'\n');
-        for i in 0..row {
-            if i == row / 2 {
-                // baby snake
-                std_out.write_all(&a).unwrap();
-                print!("\x1b[1A\x1b[{}C◼◼\n", col / 2 - 1);
-            } else {
-                std_out.write_all(&a).unwrap();
-            }
+        for _ in 0..row {
+            std_out.write_all(&a).unwrap();
             std_out.flush().unwrap();
         }
 
@@ -41,48 +24,99 @@ impl Frame {
         }
     }
 
-    pub fn write(&mut self, row: usize, col: usize, s: String) -> Result<()> {
-        if row >= self.row || col >= self.col {
+    pub fn write(&mut self, row: i32, col: i32, s: String) -> Result<()> {
+        if row < 0 || col < 0 {
             return Err(Error::new(ErrorKind::Other, "beyond boundary"));
         }
-        //let format_str = self.move_to(row, col);
+
+        if row as usize >= self.row || col as usize >= self.col {
+            return Err(Error::new(ErrorKind::Other, "beyond boundary"));
+        }
+
         if col == 0 {
             write!(
                 self.points,
                 "{}",
-                format_args!("\x1b[s\x1b[{}A\r{}\x1b[u", self.row - row, s)
+                format_args!("\x1b[s\x1b[{}A\r{}\x1b[u", self.row - row as usize, s)
             )
+            .unwrap()
         } else {
             write!(
                 self.points,
                 "{}",
-                format_args!("\x1b[s\x1b[{}A\r\x1b[{}C{}\x1b[u", self.row - row, col, s)
+                format_args!(
+                    "\x1b[s\x1b[{}A\r\x1b[{}C{}\x1b[u",
+                    self.row - row as usize,
+                    col,
+                    s
+                )
             )
+            .unwrap()
         };
         self.points.flush()
     }
 }
 
-pub fn init_stdout(row: usize, col: usize) -> Stdout {
-    let mut std_out = stdout();
-    let mut a = vec![0; col];
-    repeat(b'.').read_exact(&mut a).unwrap();
-    a.push(b'\n');
-    for i in 0..row {
-        if i == row / 2 {
-            // baby snake
-            std_out.write_all(&a).unwrap();
-            print!("\x1b[1A\x1b[{}C◼◼\n", col / 2 - 1);
-        } else {
-            std_out.write_all(&a).unwrap();
-        }
-        std_out.flush().unwrap();
-    }
-    std_out
+#[derive(Copy, Clone, Debug)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
 }
 
-pub fn back_to_origin(std_out: &mut Stdout) {
-    std_out.write(b"\x1b[s\x1b[9A\x1b[39D2\x1b[u").unwrap();
-    //std_out.write(b"\x1b[9B\x1b[39C").unwrap();
-    std_out.flush().unwrap();
+#[derive(Debug)]
+pub struct Snake {
+    body: Vec<(i32, i32)>,
+    dir: Direction,
+}
+
+impl Snake {
+    pub fn new(head: (i32, i32), dir: Direction, length: usize) -> Self {
+        let a = match dir {
+            Direction::Up => (-1, 0),
+            Direction::Down => (1, 0),
+            Direction::Left => (0, 1),
+            Direction::Right => (0, -1),
+        };
+
+        // make tail
+        let b = (0..length)
+            .map(|x| (head.0 + x as i32 * a.0, head.1 + x as i32 * a.1))
+            .collect::<Vec<_>>();
+
+        if !b.iter().all(|x| x.0 >= 0 && x.1 >= 0) {
+            return Self { body: vec![], dir };
+        }
+
+        Self {
+            body: b.iter().map(|x| (x.0, x.1)).collect::<Vec<_>>(),
+            dir,
+        }
+    }
+
+    /// can only show when the change happen, have bug of showing snake when the length > 2
+    pub fn show(&mut self, frame: &mut Frame) -> Result<()> {
+        for b in &self.body {
+            frame.write(b.0, b.1, String::from("◼"))?;
+        }
+        Ok(())
+    }
+
+    pub fn move_one_step(&mut self) -> Result<Option<(i32, i32)>> {
+        let step = match self.dir {
+            Direction::Up => (1, 0),
+            Direction::Down => (-1, 0),
+            Direction::Left => (0, -1),
+            Direction::Right => (0, 1),
+        };
+
+        let tail = self.body.pop().unwrap();
+        let a = self.body.first().unwrap();
+        let new_head = (a.0 as i32 + step.0, a.1 as i32 + step.1);
+
+        self.body.insert(0, new_head);
+
+        Ok(Some(tail))
+    }
 }
--- rusty/snake-game/src/main.rs
+++ rusty/snake-game/src/main.rs
@@ -1,16 +1,28 @@
 use snake_game::*;
-use std::{thread, time};
+use std::io::Result;
+use std::{thread::sleep, time::Duration};
 
-fn main() {
+fn main() -> Result<()> {
     let mut a = Frame::new(10, 20);
     //let mut col = 0;
     //a.write(0, 0, String::from("◼")).unwrap();
     //a.write(0, 1, String::from("◼")).unwrap();
     //a.write(0, 2, String::from("◼")).unwrap();
-    for col in 0..11 {
-        a.write(col, col, String::from("◼")).unwrap();
-        thread::sleep(time::Duration::from_secs(1));
-    }
+    // for col in 0..11 {
+    //     a.write(col, col, String::from("◼")).unwrap();
+    //     thread::sleep(time::Duration::from_secs(1));
+    // }
 
-    a.write(0, 0, String::from("a")).unwrap();
+    //a.write(0, 0, String::from("a")).unwrap();
+
+    let mut snake = Snake::new((5, 5), Direction::Right, 2);
+    println!("{:?}", snake);
+    loop {
+        snake.show(&mut a).unwrap();
+        sleep(Duration::from_secs(1));
+        match snake.move_one_step()? {
+            Some(tt) => a.write(tt.0, tt.1, String::from("."))?,
+            None => (),
+        };
+    }
 }
