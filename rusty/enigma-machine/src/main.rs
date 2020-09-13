92f38d9c7eb454e9b70667f9a6c20326ef23a877
ccQpein <ccqpein@protonmail.com>
2020-09-13 16:54:13 -0400

    a bit

--- rusty/enigma-machine/src/main.rs
+++ rusty/enigma-machine/src/main.rs
@@ -2,6 +2,9 @@ use enigma_machine::plugboard::*;
 use enigma_machine::*;
 
 fn main() {
-    let pb = Plugboard::default(); // empty
+    let pb = Plugboard::new(&vec![(1, 23), (4, 9)]);
     let mut enigma_machine = EnigmaM::new(26, 3, &pb);
+
+    // ask input
+    loop {}
 }
