61799624fb46e4b46afbcdd25fd8104cb3af2f40
ccQpein <ccqpein@protonmail.com>
2020-11-21 20:27:52 -0500

    a bit

--- rusty/Crawler/src/main.rs
+++ rusty/Crawler/src/main.rs
@@ -1,9 +1,46 @@
 use curl::easy::Easy;
+use scraper::{Html, Selector};
 use std::io::{stdout, Write};
+use Crawler::parser_chain::ParserTree;
 
 fn main() {
-    let mut handler = Easy::new();
-    let url = "https://core.telegram.org/bots/api";
+    /// example
+    let mut dst = Vec::new();
+    let mut easy = Easy::new();
+    easy.url("https://core.telegram.org/bots/api").unwrap();
 
-    handler.
+    let mut transfer = easy.transfer();
+
+    transfer
+        .write_function(|data| {
+            dst.extend_from_slice(data);
+            Ok(data.len())
+        })
+        .unwrap();
+
+    transfer.perform().unwrap();
+    drop(transfer); // Drop!
+
+    let page = String::from_utf8(dst).unwrap();
+
+    let fragment = Html::parse_fragment(&page);
+
+    let tree = ParserTree::new(&[r#"div[id="dev_page_content"]"#, r#"h3"#]).unwrap();
+    tree.select_html(&fragment).iter().for_each(|s| {
+        if s.text().collect::<Vec<_>>()[0] == "Getting updates" {
+            println!("oh yeah");
+        }
+    });
+    // let selector0 = Selector::parse(r#"div[id="dev_page_content"]"#).unwrap();
+    // let selector1 = Selector::parse(r#"h3"#).unwrap();
+
+    // let ast0 = fragment.select(&selector0).next().unwrap();
+    // for entry in ast0.select(&selector1) {
+    //     println!("{:?}", entry.text().collect::<Vec<_>>());
+    //     if entry.text().collect::<Vec<_>>()[0] == "Getting updates" {
+    //         println!("oh yeah");
+    //         return;
+    //     }
+    // }
+    // println!("oh no");
 }
---
+++ rusty/Crawler/src/parser_chain.rs
@@ -0,0 +1,70 @@
+use cssparser::ParseError;
+use scraper::{ElementRef, Html, Selector};
+use selectors::parser::SelectorParseErrorKind;
+
+#[derive(Debug)]
+pub struct ParserTree {
+    selectors: Vec<Selector>,
+}
+
+impl ParserTree {
+    pub fn new<'a>(
+        argvs: &'a [&'a str],
+    ) -> Result<Self, ParseError<'a, SelectorParseErrorKind<'a>>> {
+        let mut cache = Vec::with_capacity(argvs.len());
+        for s in argvs {
+            match Selector::parse(s) {
+                Ok(h) => cache.push(h),
+                Err(e) => return Err(e),
+            }
+        }
+
+        Ok(Self { selectors: cache })
+    }
+
+    pub fn select_html<'a>(&self, html: &'a Html) -> Vec<ElementRef<'a>> {
+        let mut a: Vec<ElementRef<'a>> = html.select(&self.selectors[0]).collect();
+        for i in 1..self.selectors.len() {
+            a = a
+                .iter()
+                .map(|ele_ref| {
+                    ele_ref
+                        .select(&self.selectors[i])
+                        .collect::<Vec<ElementRef<'a>>>()
+                })
+                .flatten()
+                .collect();
+        }
+        a
+    }
+}
+
+/// find all tags/class/etc. in one big tag.
+#[derive(Debug)]
+pub struct ParserSet {
+    selectors: Vec<Selector>,
+}
+
+impl ParserSet {
+    pub fn new<'a>(
+        argvs: &'a [&'a str],
+    ) -> Result<Self, ParseError<'a, SelectorParseErrorKind<'a>>> {
+        let mut cache = Vec::with_capacity(argvs.len());
+        for s in argvs {
+            match Selector::parse(s) {
+                Ok(h) => cache.push(h),
+                Err(e) => return Err(e),
+            }
+        }
+
+        Ok(Self { selectors: cache })
+    }
+
+    pub fn select_html<'a>(&self, html: &'a Html) -> Vec<ElementRef<'a>> {
+        self.selectors
+            .iter()
+            .map(|s| html.select(s).collect::<Vec<ElementRef<'a>>>())
+            .flatten()
+            .collect()
+    }
+}
