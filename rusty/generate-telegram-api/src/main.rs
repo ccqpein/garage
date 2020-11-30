ad66feb44b9073105ea962be44b5a8df41b40262
ccQpein <ccqpein@protonmail.com>
2020-11-30 11:18:39 -0500

    a bit trash

--- rusty/generate-telegram-api/src/main.rs
+++ rusty/generate-telegram-api/src/main.rs
@@ -1,6 +1,7 @@
 use curl::easy::Easy;
 use Crawler::parser_chain::{ParserSet, ParserTree};
-use Crawler::Html;
+use Crawler::tools::*;
+use Crawler::{ElementRef, Html, Node};
 
 fn get_telegram_html() -> Html {
     let mut dst = Vec::new();
@@ -37,9 +38,26 @@ fn main() {
     //:= TODO: need pick all tags
     //:= TODO: then use children to check
 
-    let mut doc = find_doc_parser.select_html(&fragment)[0].children();
-    for i in 0..9 {
-        println!("{:?}", doc.next().unwrap().value())
-    }
-    let find_all_tags_parser = ParserSet::new(&[r#"h4"#, r#"h3"#, r#"p"#]);
+    let mut doc = find_doc_parser.select_html(&fragment)[0];
+    let mut left_children = skip_children_after(&mut doc, |c| match c.value() {
+        Node::Element(x) => {
+            if x.name() == "p" {
+                if ElementRef::wrap(*c).unwrap().inner_html()
+                    == r##"The majority of bots will be OK with the default configuration, running on our servers. But if you feel that you need one of <a href="#using-a-local-bot-api-server">these features</a>, you're welcome to switch to your own at any time."##
+                {
+                    true
+                } else {
+                    false
+                }
+            } else {
+                false
+            }
+        }
+        _ => false,
+    }).unwrap();
+
+    let left_children = left_children.map(|nr| ElementRef::wrap(nr)).collect();
+
+    let find_all_tags_parser = ParserSet::new(&[r#"h4"#, r#"h3"#, r#"p"#, r#"table"#]);
+    find_all_tags_parser.select_ele()
 }
