a47bb020522ff1ae14c3386515e6ea38966ef814
ccQpein <ccqpein@protonmail.com>
2021-01-25 10:57:20 -0500

    a bit

--- lisp/telegram-bot/demo.lisp
+++ lisp/telegram-bot/demo.lisp
@@ -1,28 +1,37 @@
 (ql:quickload "telegram-bot")
 
+(defparameter *global-record* (make-list :size 5))
+(defparameter *token*)
+
+(defun check-if-inside-record (&optional (record *global-record*))
+  )
+
 ;;:= Use this demo for organizing code 
-(defun main ()
-  (let* ((token (read))
-         (mesg (telegram-bot::call-api-method
-                (car telegram-bot::*all-methods*)
-                :token token
-                ;;:offset -1
+(defun handle ()
+  (let* ((mesg (telegram-bot:call-api-method
+                (get-method "getUpdates")
+                :token *token*
+                :offset -10
                 ))
-         (obj (telegram-bot::parse-message mesg))
+         (obj (telegram-bot:parse-message mesg))
+         (all-results (telegram-bot:get-value-with-keys
+                       obj
+                       '("result")))
          )
-    (if (string= (caar (telegram-bot::get-value-with-keys
+    ;; stop here
+    (if (string= (caar (telegram-bot:get-value-with-keys
                         obj
                         '("result" "message" "entities" "type")))
                  "bot_command")
-        (telegram-bot::call-api-method
-         (telegram-bot::get-method "sendMessage")
-         :token token
+        (telegram-bot:call-api-method
+         (telegram-bot:get-method "sendMessage")
+         :token *token*
          :chat_id (car
-                   (telegram-bot::get-value-with-keys
+                   (telegram-bot:get-value-with-keys
                     obj
                     '("result" "message" "chat" "id")))
          :text (concatenate 'string "hello, "
                             (car
-                             (telegram-bot::get-value-with-keys
+                             (telegram-bot:get-value-with-keys
                               obj
                               '("result" "message" "chat" "first_name"))))))))
--- lisp/telegram-bot/telegram-bot.lisp
+++ lisp/telegram-bot/telegram-bot.lisp
@@ -3,7 +3,17 @@
         #:tele-api-doc
         #:message-handler
         #:telegram-client
-        ))
+        )
+  (:export #:call-api-method
+           #:parse-message
+           #:get-value-with-keys
+           #:call-api-method
+           #:read-token
+           #:get-datatype
+           #:get-method
+           
+           ;;#:*telegram-api-url*
+           ))
 ;;:= TODO: need export symbols here...
 ;;:= and clean other packages' export symbols.
 
