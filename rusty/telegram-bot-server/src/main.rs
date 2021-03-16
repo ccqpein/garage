4a23dd39745dbb8c4504eb280b6a9f3631a1347d
ccQpein <ccqpein@protonmail.com>
2021-03-16 09:50:34 -0400

    abit

--- rusty/telegram-bot-server/src/main.rs
+++ rusty/telegram-bot-server/src/main.rs
@@ -1,17 +1,12 @@
-use actix_web::{
-    error, post, web, App, Error, FromRequest, HttpRequest, HttpResponse, HttpServer, Responder,
+use actix_web::{error, web, App, Error, HttpResponse, HttpServer};
+use futures::StreamExt;
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+use telegram_bot::{
+    types::{requests::SendMessage, MessageKind, Update, UpdateKind},
+    Api,
 };
 
-use futures::StreamExt;
-use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod, SslStream};
-use std::io;
-use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
-use std::sync::Arc;
-use std::thread;
-use telegram_bot::types::Update;
-
-async fn index(mut payload: web::Payload) -> Result<HttpResponse, Error> {
+async fn handler(mut payload: web::Payload, api: web::Data<Api>) -> Result<HttpResponse, Error> {
     // parse json now
     let mut body = web::BytesMut::new();
     while let Some(chunk) = payload.next().await {
@@ -24,58 +19,66 @@ async fn index(mut payload: web::Payload) -> Result<HttpResponse, Error> {
         body.extend_from_slice(&chunk);
     }
 
-    println!(
-        "receive {}",
-        String::from_utf8(body.to_vec()).map_err(|e| error::ErrorBadRequest(e.to_string()))?
-    );
+    let update = serde_json::from_slice::<Update>(&body)?;
 
-    println!(
-        "{:?}",
-        //String::from_utf8(body.to_vec()).map_err(|e| error::ErrorBadRequest(e.to_string()))?
-        serde_json::from_slice::<Update>(&body)?
-    );
+    if let UpdateKind::Message(message) = update.kind {
+        if let MessageKind::Text { ref data, .. } = message.kind {
+            // Print received text message to stdout.
+            println!("<{}>: {}", &message.from.first_name, data);
 
-    Ok(HttpResponse::Ok().body("ok"))
-}
-
-async fn index2(mut payload: web::Payload) -> Result<HttpResponse, Error> {
-    // parse json now
-    let mut body = web::BytesMut::new();
-    while let Some(chunk) = payload.next().await {
-        let chunk = chunk.map_err(|e| error::ErrorBadRequest(e.to_string()))?;
-
-        if (body.len() + chunk.len()) > 262_144_usize {
-            return Err(error::ErrorBadRequest(""));
+            // Answer message with "Hi".
+            api.send(SendMessage::new(
+                message.chat,
+                format!(
+                    "Hi, {}! You just wrote '{}'",
+                    &message.from.first_name, data
+                ),
+            ))
+            .await
+            .map_err(|_| Error::from(HttpResponse::BadRequest()))?;
         }
-
-        body.extend_from_slice(&chunk);
     }
 
-    println!(
-        "{:?}",
-        //String::from_utf8(body.to_vec()).map_err(|e| error::ErrorBadRequest(e.to_string()))?
-        serde_json::from_slice::<Update>(&body)?
-    );
-
-    Ok(HttpResponse::Ok().body("ok"))
+    Ok(HttpResponse::Ok().body(""))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // SSL builder
     let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    // read private key
     builder
         .set_private_key_file("./vault/key.pem", SslFiletype::PEM)
         .unwrap();
+    // read certificate
     builder
         .set_certificate_chain_file("./vault/certs.pem")
         .unwrap();
 
+    // declare endpoint
     let endpoint = include_str!("../vault/endpoint");
 
-    //////////////////////
+    // read token
+    let token = include_str!("../vault/telebottoken")
+        .lines()
+        .next()
+        .unwrap();
 
-    HttpServer::new(move || App::new().route(endpoint, web::post().to(index2)))
-        .bind_openssl("0.0.0.0:8443", builder)?
-        .run()
-        .await
+    // tracing
+    tracing::subscriber::set_global_default(
+        tracing_subscriber::FmtSubscriber::builder()
+            .with_env_filter("telegram_bot=trace")
+            .finish(),
+    )
+    .unwrap();
+
+    // start http server
+    HttpServer::new(move || {
+        App::new()
+            .data(Api::new(token))
+            .route(endpoint, web::post().to(handler))
+    })
+    .bind_openssl("0.0.0.0:8443", builder)?
+    .run()
+    .await
 }
