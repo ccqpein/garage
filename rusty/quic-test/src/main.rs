dc4d1a53fb25cdcf5908439d00996a5f5bea73a3
ccQpein <ccqpein@protonmail.com>
2021-05-12 08:38:28 -0400

    a bit

--- rusty/quic-test/src/main.rs
+++ rusty/quic-test/src/main.rs
@@ -1,27 +1,12 @@
+use log::{debug, error, info};
+
 use quiche::{Config, ConnectionId};
 use std::net;
 
-// pub struct Client {
-//     pub conn: std::pin::Pin<Box<quiche::Connection>>,
+fn main() -> ! {
+    env_logger::builder().format_timestamp_nanos().init();
 
-//     pub http_conn: Option<Box<dyn HttpConn>>,
-
-//     pub siduck_conn: Option<SiDuckConn>,
-
-//     pub app_proto_selected: bool,
-
-//     pub partial_requests: std::collections::HashMap<u64, PartialRequest>,
-
-//     pub partial_responses: std::collections::HashMap<u64, PartialResponse>,
-// }
-
-// pub type ClientMap = HashMap<ConnectionId<'static>, (net::SocketAddr, Client)>;
-
-fn main() {
-    env_logger::builder()
-        .default_format_timestamp_nanos(true)
-        .init();
-
+    // mio event poll
     let poll = mio::Poll::new().unwrap();
     let mut events = mio::Events::with_capacity(1024);
 
@@ -37,6 +22,26 @@ fn main() {
     .unwrap();
 
     let mut pkt_count = 0;
+    let mut buf = [0; 65535];
+
+    loop {
+        // wait until events coming
+        poll.poll(&mut events, None).unwrap();
+
+        let (len, src) = match socket.recv_from(&mut buf) {
+            Ok(v) => v,
+
+            Err(e) => {
+                // There are no more UDP packets to read, so end the read
+                // loop.
+                if e.kind() == std::io::ErrorKind::WouldBlock {
+                    info!("recv() would block");
+                }
+
+                panic!("recv() failed: {:?}", e);
+            }
+        };
+    }
 
     // let config = Config::new(quiche::PROTOCOL_VERSION)?;
     // let conn = quiche::accept(&scid, None, &mut config)?;
