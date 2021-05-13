72d9acb5dcf570afcba50cafb86bd5512ef2e126
ccQpein <ccqpein@protonmail.com>
2021-05-13 10:32:36 -0400

    a bit

--- rusty/quic-test/src/main.rs
+++ rusty/quic-test/src/main.rs
@@ -1,6 +1,7 @@
-use log::{debug, error, info};
+use log::{debug, error, info, trace};
 
 use quiche::{Config, ConnectionId};
+use ring::rand::*;
 use std::net;
 
 fn main() -> ! {
@@ -21,8 +22,13 @@ fn main() -> ! {
     )
     .unwrap();
 
+    //:= don't know what are these
+    let rng = SystemRandom::new();
+    let conn_id_seed = ring::hmac::Key::generate(ring::hmac::HMAC_SHA256, &rng).unwrap();
+
     let mut pkt_count = 0;
-    let mut buf = [0; 65535];
+    let mut buf = [0; 1024];
+    let mut out = [0; 1024];
 
     loop {
         // wait until events coming
@@ -41,6 +47,52 @@ fn main() -> ! {
                 panic!("recv() failed: {:?}", e);
             }
         };
+
+        let pkt_buf = &mut buf[..len];
+        info!("buf now {:?}", &pkt_buf);
+
+        // parser header
+        let hdr = match quiche::Header::from_slice(pkt_buf, quiche::MAX_CONN_ID_LEN) {
+            Ok(v) => v,
+
+            Err(e) => {
+                panic!("Parsing packet header failed: {:?}", e);
+            }
+        };
+
+        trace!("got packet {:?}", hdr);
+
+        // conn id maker
+        let conn_id = ring::hmac::sign(&conn_id_seed, &hdr.dcid);
+        let conn_id = &conn_id.as_ref()[..quiche::MAX_CONN_ID_LEN];
+        let conn_id: ConnectionId = conn_id.to_vec().into();
+
+        // make new client
+        if hdr.ty != quiche::Type::Initial {
+            panic!("Packet is not Initial");
+        }
+
+        // header version
+        if !quiche::version_is_supported(hdr.version) {
+            info!("Doing version negotiation");
+
+            let len = quiche::negotiate_version(&hdr.scid, &hdr.dcid, &mut out).unwrap();
+
+            let out = &out[..len];
+
+            if let Err(e) = socket.send_to(out, &src) {
+                if e.kind() == std::io::ErrorKind::WouldBlock {
+                    trace!("send() would block");
+                }
+
+                panic!("send() failed: {:?}", e);
+            }
+        }
+
+        let mut scid = [0; quiche::MAX_CONN_ID_LEN];
+        scid.copy_from_slice(&conn_id);
+
+        let mut odcid: Option<ConnectionId> = None;
     }
 
     // let config = Config::new(quiche::PROTOCOL_VERSION)?;
