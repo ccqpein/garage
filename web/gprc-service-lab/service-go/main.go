ff968058103585b605ee96a22dfeec89809c4aaf
ccQpein <ccqpein@protonmail.com>
2021-06-15 09:10:15 -0400

    a bit

--- web/gprc-service-lab/service-go/main.go
+++ web/gprc-service-lab/service-go/main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"log"
+	pd "service-go/protocols/hello"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("127.0.0.1:9090", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pd.NewHelloWorldClient(conn)
+
+}
