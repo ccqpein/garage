b6f7bbec0c8870468089ad81dd6fd910c3e4fe34
ccQpein <ccqpein@protonmail.com>
2021-07-07 00:02:33 -0400

    a bit

--- rusty/functor/src/main.rs
+++ rusty/functor/src/main.rs
@@ -2,14 +2,54 @@ struct A<T> {
     inner: T,
 }
 
-impl Functor<i32, i32> for A<i32> {
-    fn fmap(&self, f: &dyn Fn(i32) -> i32) -> i32 {
-        f(self.inner)
+// method 1:
+// impl Functor<i32, i32> for A<i32> {
+//     fn fmap(&self, f: &dyn Fn(i32) -> i32) -> i32 {
+//         f(self.inner)
+//     }
+// }
+
+// trait Functor<T1, T2> {
+//     fn fmap(&self, f: &dyn Fn(T1) -> T2) -> T2;
+// }
+
+// method 2:
+// impl Functor for A<i32> {
+//     type T = i32;
+//     fn fmap<F, B>(&self, f: F) -> B
+//     where
+//         F: Fn(i32) -> B,
+//     {
+//         f(self.inner)
+//     }
+// }
+
+// trait Functor {
+//     type T;
+//     fn fmap<F, B>(&self, f: F) -> B
+//     where
+//         F: Fn(Self::T) -> B;
+// }
+
+// method 3
+trait Functor {
+    type T;
+    fn fmap<'a, F, B>(&'a self, f: F) -> B
+    where
+        F: Fn(&'a Self::T) -> B;
+}
+
+impl<S> Functor for A<S> {
+    type T = S;
+    fn fmap<'a, F, B>(&'a self, f: F) -> B
+    where
+        F: Fn(&'a S) -> B,
+    {
+        f(&self.inner)
     }
 }
 
-trait Functor<T1, T2> {
-    fn fmap(&self, f: &dyn Fn(T1) -> T2) -> T2;
+fn main() {
+    let a = A { inner: 2 };
+    assert_eq!(a.fmap(|x| *x + 2), 4);
 }
-
-fn main() {}
