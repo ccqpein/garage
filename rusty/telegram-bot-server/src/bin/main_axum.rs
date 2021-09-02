c56a3007ceb551bc57cef7859e454e9644e64fbf
ccQpein <ccqpein@protonmail.com>
2021-09-01 20:59:36 -0400

    a hammer

--- rusty/telegram-bot-server/src/bin/main_axum.rs
+++ rusty/telegram-bot-server/src/bin/main_axum.rs
@@ -1,1 +1,8 @@
-fn main() {}
+fn main() {
+    // expand main macro
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {})
+}
