deb43d1ae1f2ccbc48bcac299e4b600372f8ed58
ccQpein <ccqpein@protonmail.com>
2021-09-23 10:57:18 -0400

    a hammer

--- rusty/sudoku-solver/src/lib.rs
+++ rusty/sudoku-solver/src/lib.rs
@@ -1,3 +1,32 @@
 struct Cell<T: PartialEq> {
     inner: T,
 }
+
+enum Crowd<T: PartialEq> {
+    Cell(Cell<T>),
+    Grid(Box<Grid<T>>),
+}
+
+struct Potential<T: PartialEq> {
+    /// store value and if it has picked before
+    inner: Vec<(T, bool)>,
+}
+
+struct Grid<T: PartialEq> {
+    data: [[Crowd<T>; 3]; 3],
+    potential_records: [[Potential<T>; 3]; 3],
+}
+
+impl<T: PartialEq> Grid<T> {
+    /// only if crowd inside is Cell
+    fn mutex_all(&self) {
+        //:= TODO
+        todo!()
+    }
+}
+
+fn solve<T>(g: &Grid<T>)
+where
+    T: PartialEq,
+{
+}
