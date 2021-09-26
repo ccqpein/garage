aed49dc1363e9e1c487eaa176dd62ebb42055d3e
ccQpein <ccqpein@protonmail.com>
2021-09-26 12:49:57 -0400

    a hammer

--- rusty/sudoku-solver/src/lib.rs
+++ rusty/sudoku-solver/src/lib.rs
@@ -3,8 +3,24 @@ struct Cell<T: PartialEq> {
 }
 
 enum Crowd<T: PartialEq> {
-    Cell(Cell<T>),
+    Cell(Option<Cell<T>>),
     Grid(Box<Grid<T>>),
+}
+
+impl<T: PartialEq> Crowd<T> {
+    fn is_cell(&self) -> bool {
+        match self {
+            Self::Cell(_) => true,
+            _ => false,
+        }
+    }
+
+    fn is_grid(&self) -> bool {
+        match self {
+            Self::Grid(_) => true,
+            _ => false,
+        }
+    }
 }
 
 struct Potential<T: PartialEq> {
@@ -13,13 +29,16 @@ struct Potential<T: PartialEq> {
 }
 
 struct Grid<T: PartialEq> {
-    data: [[Crowd<T>; 3]; 3],
+    data: [[Crowd<T>; 3]; 3], // 3 * 3
     potential_records: [[Potential<T>; 3]; 3],
 }
 
 impl<T: PartialEq> Grid<T> {
-    /// only if crowd inside is Cell
-    fn mutex_all(&self) {
+    /// only if crowd insided is Cell
+    fn mutex_all(&self) -> Result<(), String> {
+        if !self.data[0][0].is_cell() {
+            return Err("grid should be cells".to_string());
+        }
         //:= TODO
         todo!()
     }
