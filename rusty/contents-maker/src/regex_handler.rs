9bff0ae130c3d5119783a6610146e6a17cacaf2c
ccQpein <ccqpein@protonmail.com>
2021-10-15 10:02:33 -0400

    a hammer

--- rusty/contents-maker/src/regex_handler.rs
+++ rusty/contents-maker/src/regex_handler.rs
@@ -1,25 +1,35 @@
 use lazy_static::*;
-use regex::Regex;
+use regex::{Captures, Match, Regex};
 
-//const TITLE_REGEX_STR: &'static str = r"^(?P<HEAD>#+\s.*)";
-const TITLE_REGEX_STR: &'static str = r"^(?P<HEAD>#+\s*)\w+";
+const TITLE_REGEX_STR: &'static str = r"^(?P<HEAD>#+\s*)(?P<CONTENT>[^#]*)";
 
 lazy_static! {
     static ref TITLE_REGEX: Regex = Regex::new(TITLE_REGEX_STR).unwrap();
 }
 
-fn pick_the_head(content: &str) -> Option<regex::Match> {
-    // match TITLE_REGEX.captures(content) {
-    //     Some(cap) => cap.name("HEAD"),
-    //     None => None,
-    // }
+fn capture_title(s: &str) -> Option<Captures> {
+    TITLE_REGEX.captures(s)
+}
 
-    TITLE_REGEX.captures(content).unwrap().name("HEAD")
+/// match the # symbols in the front of title
+fn pick_the_head<'t>(cap: &Captures<'t>) -> Result<Match<'t>, String> {
+    cap.name("HEAD").ok_or("Cannot find HEAD group.".into())
+}
+
+/// count how many # in this title match
+fn head_count(m: &Match) -> usize {
+    m.as_str().chars().filter(|c| *c == '#').count()
+}
+
+fn pick_the_head_content<'t>(cap: &Captures<'t>) -> Result<String, String> {
+    cap.name("CONTENT")
+        .ok_or("Cannot find CONTENT group.".into())
+        .map(|m| m.as_str().into())
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::regex_handler::pick_the_head;
+    use super::*;
 
     #[test]
     fn test_match_the_title() {
@@ -32,7 +42,61 @@ mod tests {
         ];
 
         for case in testcasees {
-            dbg!(pick_the_head(case));
+            //dbg!(pick_the_head(case));
+            assert!(pick_the_head(&capture_title(case).unwrap()).is_ok())
         }
+    }
+
+    #[test]
+    fn test_head_count() {
+        let testcase = "#README";
+        assert_eq!(
+            head_count(&pick_the_head(&capture_title(testcase).unwrap()).unwrap()),
+            1
+        );
+
+        let testcase = "## README";
+        assert_eq!(
+            head_count(&pick_the_head(&capture_title(testcase).unwrap()).unwrap()),
+            2
+        );
+
+        let testcase = "### README #";
+        assert_eq!(
+            head_count(&pick_the_head(&capture_title(testcase).unwrap()).unwrap()),
+            3
+        );
+
+        let testcase = "# README#";
+        assert_eq!(
+            head_count(&pick_the_head(&capture_title(testcase).unwrap()).unwrap()),
+            1
+        );
+
+        let testcase = "# #   README #";
+        assert_eq!(
+            head_count(&pick_the_head(&capture_title(testcase).unwrap()).unwrap()),
+            1
+        );
+    }
+
+    #[test]
+    fn test_pick_the_head_content() {
+        let title = capture_title("#README").unwrap();
+        assert_eq!(pick_the_head_content(&title), Ok("README".to_string()));
+
+        let title = capture_title("## README").unwrap();
+        assert_eq!(pick_the_head_content(&title), Ok("README".to_string()));
+
+        let title = capture_title("# README").unwrap();
+        assert_eq!(pick_the_head_content(&title), Ok("README".to_string()));
+
+        // cannot accept # before readme
+        let title = capture_title("# #  README #").unwrap();
+        assert_eq!(pick_the_head_content(&title), Ok("".to_string()));
+
+        // last space
+        let title = capture_title("# README ##").unwrap();
+        assert_eq!(pick_the_head_content(&title), Ok("README ".to_string()));
     }
 }
