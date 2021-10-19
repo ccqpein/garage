b8d64784684c89b2e8f14a944f422cdb55efa99d
ccQpein <ccqpein@protonmail.com>
2021-10-19 00:02:13 -0400

    a hammer

--- lisp/contents-makeer.lisp
+++ lisp/contents-makeer.lisp
@@ -1,25 +1,48 @@
 ;;; same stuff as ../../rusty/contents-maker
-(ql:quickload "cl-ppcre" "str")
+(ql:quickload '("cl-ppcre" "str"))
 
 (defvar *title-regex* "^(?<head>#+\\s*)(?<content>[^#]*)")
 
 ;; for clean the # symbols inside like ```
 (defvar *special-block-sym-stack* '())
 
+(defun code-block? (line)
+  (if (cl-ppcre:scan "^```\\w*" line)
+      'code-block
+      nil))
+
+(defun block-sym-stack-top ()
+  (car *special-block-sym-stack*))
+
 (defun regex-scan-title (title)
   "give title and return the sharp number and content"
   (let* ((cl-ppcre:*allow-named-registers* t))
-    (let ((result (cadr (multiple-value-list (cl-ppcre:scan-to-strings *title-regex* title)))))
-      (if result
-          (let ((sharps (elt result 0))
-                (content (elt result 1)))
-            (values t
-                    (list (count #\# sharps)
-                          (string-right-trim " " content))))
-          (values nil nil)))
+    (if (block-sym-stack-top) ;; in code block stack
+        (if (code-block? title)
+            ;; remove from stack
+            (progn
+              (pop *special-block-sym-stack*)
+              (values nil nil))
+            ;; return
+            (values nil nil))
+        (if (code-block? title)
+            (progn
+              (push 'code-block *special-block-sym-stack*)
+              (values nil nil))
+            
+            ;; normal parse below
+            (let ((result (cadr (multiple-value-list (cl-ppcre:scan-to-strings *title-regex* title)))))
+              (if result
+                  (let ((sharps (elt result 0))
+                        (content (elt result 1)))
+                    (values t
+                            (list (count #\# sharps)
+                                  (string-right-trim " " content))))
+                  (values nil nil)))))
     ))
 
 (defun handle-file (filepath)
+  (setf *special-block-sym-stack* '())
   (with-open-file (f filepath :direction :input)
     (loop
       for line = (read-line f nil nil)
@@ -37,7 +60,7 @@
                             "~a-[~a](~a)~%"
                             (format nil "~v@{ ~}" count "")
                             content
-                            (str:concat "#" (str:replace-all " " "-" content))
+                            (string-downcase (str:concat "#" (str:replace-all " " "-" content)))
                             ))
                ))
       )))
