259c2a0dc61631524b784fe7f2daea18614f1506
ccQpein <ccqpein@protonmail.com>
2022-02-15 01:03:18 -0500

    add some tiny stuff

--- rusty/telegram-bot-server-v2/src/app/reminder.rs
+++ rusty/telegram-bot-server-v2/src/app/reminder.rs
@@ -20,6 +20,7 @@ lazy_static! {
     };
 }
 
+/// add reminder to this chat window
 async fn add_reminder(
     chatid: ChatId,
     time: u64,
@@ -41,16 +42,16 @@ async fn add_reminder(
         loop {
             sleep(Duration::from_secs(time)).await;
             match rev.try_recv() {
-                Ok(_) => return,
-                Err(TryRecvError::Empty) => continue,
-                Err(TryRecvError::Closed) => return,
+                Err(TryRecvError::Empty) => {
+                    deliver_sender.send(dlvr_msg.clone()).await;
+                }
+                _ => return,
             }
-            deliver_sender.send(dlvr_msg.clone()).await;
         }
     });
 }
 
-fn delete_reminder(chatid: &ChatId, ind: &usize) {
+async fn delete_reminder(chatid: &ChatId, ind: &usize, deliver_sender: Sender<Msg2Deliver>) {
     let mut table = REMINDERS_TABLE.lock().unwrap();
     let reminders = table.entry(*chatid).or_insert(HashMap::new());
 
@@ -59,7 +60,12 @@ fn delete_reminder(chatid: &ChatId, ind: &usize) {
             sig.send(true);
         }
         None => {
-            //:= tracing here maybe
+            let dlvr_msg = Msg2Deliver::new(
+                "send".to_string(),
+                *chatid,
+                format!("cannot find number {} reminder", ind),
+            );
+            deliver_sender.send(dlvr_msg).await;
         }
     };
 }
@@ -78,6 +84,16 @@ pub enum ReminderComm {
     MakeReminder(String, ReminderTime),
     /// cancel the reminder
     CancelReminder(usize),
+}
+
+pub struct ReminderInputConsumer {
+    snd: Sender<ReminderInput>,
+}
+
+impl ReminderInputConsumer {
+    pub fn new(snd: Sender<ReminderInput>) -> Self {
+        Self { snd }
+    }
 }
 
 /// input generated from message
@@ -173,12 +189,15 @@ impl Reminder {
                     )
                     .await
                 }
-                ReminderComm::CancelReminder(ind) => delete_reminder(&rem_input.chat_id, ind),
+                ReminderComm::CancelReminder(ind) => {
+                    delete_reminder(&rem_input.chat_id, ind, self.deliver_sender.clone()).await
+                }
             }
         }
     }
 }
 
+/// this part is the guard for checking if reminder content has sent or not
 async fn awaiting_reminder(
     status_snd: Sender<StatusCheckerInput>,
     chat_id: ChatId,
@@ -209,8 +228,14 @@ async fn awaiting_reminder(
             //:= after reminder created, status is none
             return;
         }
-        Err(_) => todo!(),
-        _ => todo!(),
+        Err(e) => {
+            debug!("awaiting reminder checking status has issue {:?}", e);
+            return;
+        }
+        _ => {
+            debug!("awaiting reminder checking status has issue: unsupport reply");
+            return;
+        }
     };
 
     sleep(Duration::from_secs(10)).await;
@@ -233,22 +258,57 @@ async fn awaiting_reminder(
                 chat_id,
                 "Run out remind waiting time".to_string(),
             ));
+
+            status_snd.send(
+                StatusCheckerInput::new(
+                    chat_id,
+                    ChatStatus::None, // just for placeholder
+                    Operate::Delete,
+                    None,
+                )
+                .unwrap(),
+            );
         }
         Ok(ChatStatus::None) => {
-            //:= after reminder created, status is none
+            //after reminder created, status is none
             return;
         }
-        Err(_) => todo!(),
-        _ => todo!(),
+        Err(e) => {
+            debug!("awaiting reminder checking status has issue {:?}", e);
+            return;
+        }
+        _ => {
+            debug!("awaiting reminder checking status has issue: unsupport reply");
+            return;
+        }
     };
+}
 
-    status_snd.send(
-        StatusCheckerInput::new(
-            chat_id,
-            ChatStatus::None, // just for placeholder
-            Operate::Delete,
-            None,
-        )
-        .unwrap(),
-    );
+/// impl app below
+
+#[async_trait]
+impl AppConsumer for ReminderInputConsumer {
+    async fn consume_msg<'a>(&mut self, msg: &'a Message) -> Result<ConsumeStatus, String> {
+        match ReminderInput::from_msg(msg) {
+            Some(input) => {
+                self.snd.send(input).await; //:= handle the error?
+                Ok(ConsumeStatus::Taken)
+            }
+            None => Ok(ConsumeStatus::NotMine),
+        }
+    }
+}
+
+#[async_trait]
+impl App for Reminder {
+    type Consumer = ReminderInputConsumer;
+
+    fn consumer(&self) -> Self::Consumer {
+        ReminderInputConsumer::new(self.sender.clone())
+    }
+
+    async fn run(mut self) -> Result<(), String> {
+        self.run().await;
+        Ok(())
+    }
 }
