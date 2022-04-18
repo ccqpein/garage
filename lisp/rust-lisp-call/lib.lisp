e24f809579644631a6373cd9199a485724407614
ccQpein <ccqpein@protonmail.com>
2022-04-18 00:03:12 -0400

    looks like some incompatible in sbcl version

--- lisp/rust-lisp-call/lib.lisp
+++ lisp/rust-lisp-call/lib.lisp
@@ -7,13 +7,13 @@
   (apply #'+ l))
 
 ;;; in cl-user in sbcl
-(define-alien-callable calc-parse int ((x c-int) (y c-int) (result (* (* t))))
+(define-alien-callable calc-add int ((x c-int) (y c-int) (result (* (* t))))
   (handler-case
       (progn
-         ;; The following needs to use SBCL internal functions to
-         ;; coerce a Lisp object into a raw pointer value. This is
-         ;; unsafe and will be fixed in the next section.
+        ;; deref:
+        ;; sap-alien:
+        ;; int-sap:
         (setf (deref result)
-              (sap-alien (int-sap (get-lisp-obj-address (parse source))) (* t)))
+              (sap-alien (int-sap (sb-kernel:get-lisp-obj-address (add x y))) (* t)))
          0)
-    (t (condition) (declare (ignore condition)) 1)))
+    (t (condition) (declare (ignore condition)) 0)))
