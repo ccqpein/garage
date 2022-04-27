56b550f27e39b7b977fae66913d09425136e6ed3
ccQpein <ccqpein@protonmail.com>
2022-04-26 21:18:43 -0400

    I am done

--- rusty/diesel_demo/README.md
+++ rusty/diesel_demo/README.md
@@ -1,3 +1,7 @@
 # README #
 
 the demo project for playing around [diesel](https://diesel.rs/guides/getting-started)
+
+## TIPS ##
+
+The `schema.rs` changed when the sql files in migrations changed. don't need to change by myself
--- rusty/diesel_demo/migrations/2022-04-24-023027_create_posts/up.sql
+++ rusty/diesel_demo/migrations/2022-04-24-023027_create_posts/up.sql
@@ -1,7 +1,7 @@
 -- Your SQL goes here
 CREATE TABLE posts (
-  --id SERIAL PRIMARY KEY, -- change a bit
-  id integer PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
+  --id integer PRIMARY KEY, -- if it is integer, I have to add it in create_post function manully
   some_uuid UUID NOT NULL,
   title VARCHAR NOT NULL,
   body TEXT NOT NULL,
