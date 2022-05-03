9aa33ddda3e4fdd1e1246df8efd3af37669556cc
ccQpein <ccqpein@protonmail.com>
2022-05-03 08:55:38 -0400

    a bit

--- lisp/macchiato/lisp/recipe.lisp
+++ lisp/macchiato/lisp/recipe.lisp
@@ -4,9 +4,30 @@
 
 (in-package #:macchiato.recipe)
 
+;; name -> table of versions
+(defvar *all-recipes* (make-hash-table :test 'equal))
+
 (defclass recipe ()
   ((name
     :initarg :name
     :type string)
    (version
-    :initarg :version)))
+    :initarg :version
+    :type string)))
+
+(defun check-recipe-version (name version)
+  "if this recipe name doesn't has this version, return true"
+  (cond ((not (gethash name *all-recipes*))
+         (setf (gethash name *all-recipes*) (make-hash-table :test 'equal))
+         t)
+        ((not (gethash version (gethash name *all-recipes*)))
+         t)
+        (t nil)))
+
+(defmethod initialize-instance :after ((r recipe) &key)
+  "after make-instance of recipt, register it inside all recipes"
+  (with-slots (name version) r
+    (assert (check-recipe-version name version)
+            nil
+            "~a version ~a has existed in *all-recipes*" name version)
+    (setf (gethash version (gethash name *all-recipes* (make-hash-table))) t)))
--- lisp/macchiato/recipes/sbcl.lisp
+++ lisp/macchiato/recipes/sbcl.lisp
@@ -1,17 +1,14 @@
 ;;; sbcl recipes for installing sbcl on mac
-(in-package #:cl-user)
+;; (in-package #:cl-user)
 
-(defpackage #:sbcl-recipe
-  (:use #:CL #:macchiato.recipe))
+;; (defpackage #:sbcl-recipe
+;;   (:use #:CL #:macchiato.recipe))
 
-(in-package #:sbcl-recipe)
+(in-package #:recipe)
 
 ;;; inherit the super class "recipe" and make a instance named sbcl-recipe
 ;;; then export it.
 ;;; then I can call it outside
-
-;; (defclass sbcl-recipe (recipe)
-;;   ((name :)))
 
 (make-instance 'recipe
                :name "sbcl-recipe"
