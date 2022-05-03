f6a012e0b7d2e49fd2a6ccf003584dcbd9d8fd12
ccQpein <ccqpein@protonmail.com>
2022-05-02 21:01:58 -0400

    a bit

---
+++ lisp/macchiato/lisp/homebrew.lisp
@@ -0,0 +1,9 @@
+;;; homebrew package for taking over homebrew tools
+(defpackage #:macchiato.homebrew-handler
+  (:use #:CL))
+
+(in-package #:macchiato.homebrew-handler)
+
+(defun run-homebrew (&rest args)
+  ;;:= todo  
+  )
---
+++ lisp/macchiato/macchiato.lisp
@@ -0,0 +1,8 @@
+(defun main (args)
+  ;;:= 1. parse args
+  ;;:= 2. check if there is recipt of this package/packages
+  ;;:= 3. run homebrew for package not exist
+  ;;:= 4. run macchiato for packages have recipe
+  )
+
+(main)
