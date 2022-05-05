c2b717504183ec0f845054a740d3b0df4a776692
ccQpein <ccqpein@protonmail.com>
2022-05-04 23:23:14 -0400

    some ideas

--- lisp/macchiato/macchiato.lisp
+++ lisp/macchiato/macchiato.lisp
@@ -1,8 +1,15 @@
 (defun main (args)
   ;;:= 1. parse args
-  ;;:= 2. check if there is recipt of this package/packages
+  ;;:= 2. check if there is recipt of
   ;;:= 3. run homebrew for package not exist
   ;;:= 4. run macchiato for packages have recipe
+  ;;; for example, I am installing sbcl
+  (load "recipes/sbcl.lisp") ;; or fasl
+  ;;:= todo: need sbcl-recipe class
+  (let ((r (make-instance 'sbcl-recipe)) ;; args maybe input as class arguments here
+        )
+    (run r) ;;:= run method of recipe and sbcl-recipr
+    ) 
   )
 
 (main)
