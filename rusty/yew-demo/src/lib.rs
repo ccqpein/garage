125591ff8a0b2a5f0df096a4737c95abf4d89890
ccQpein <ccqpein@protonmail.com>
2022-05-13 23:11:35 -0400

    a bit

--- rusty/yew-demo/src/lib.rs
+++ rusty/yew-demo/src/lib.rs
@@ -1,47 +1,88 @@
-use wasm_bindgen::prelude::*;
+// use wasm_bindgen::prelude::*;
+// use yew::prelude::*;
+
+// struct Model {
+//     link: ComponentLink<Self>,
+//     value: i64,
+// }
+
+// enum Msg {
+//     AddOne,
+// }
+
+// impl Component for Model {
+//     type Message = Msg;
+//     type Properties = ();
+//     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+//         Self { link, value: 0 }
+//     }
+
+//     fn update(&mut self, msg: Self::Message) -> ShouldRender {
+//         match msg {
+//             Msg::AddOne => self.value += 1,
+//         }
+//         true
+//     }
+
+//     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+//         // Should only return "true" if new properties are different to
+//         // previously received properties.
+//         // This component has no properties so we will always return "false".
+//         false
+//     }
+
+//     fn view(&self) -> Html {
+//         html! {
+//             <div>
+//                 <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
+//                 <p>{ self.value }</p>
+//             </div>
+//         }
+//     }
+// }
+
+// #[wasm_bindgen(start)]
+// pub fn run_app() {
+//     App::<Model>::new().mount_to_body();
+// }
+
 use yew::prelude::*;
 
-struct Model {
-    link: ComponentLink<Self>,
-    value: i64,
+pub enum Msg {
+    AddOne,
 }
 
-enum Msg {
-    AddOne,
+pub struct Model {
+    value: i64,
 }
 
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
-    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self { link, value: 0 }
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { value: 0 }
     }
 
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::AddOne => self.value += 1,
+            Msg::AddOne => {
+                self.value += 1;
+                // the value has changed so we need to
+                // re-render for it to appear on the page
+                true
+            }
         }
-        true
     }
 
-    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        // Should only return "true" if new properties are different to
-        // previously received properties.
-        // This component has no properties so we will always return "false".
-        false
-    }
-
-    fn view(&self) -> Html {
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
+        let link = ctx.link();
         html! {
             <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
+                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
                 <p>{ self.value }</p>
             </div>
         }
     }
-}
-
-#[wasm_bindgen(start)]
-pub fn run_app() {
-    App::<Model>::new().mount_to_body();
 }
---
+++ rusty/yew-demo/src/main.rs
@@ -0,0 +1,5 @@
+use yew_demo::*;
+
+fn main() {
+    yew::start_app::<Model>();
+}
