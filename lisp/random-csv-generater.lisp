8103d520b0d1a933c25156248749013781d83864
ccQpein <ccqpein@protonmail.com>
2022-05-28 02:44:32 -0400

    a bit

---
+++ lisp/random-csv-generater.lisp
@@ -0,0 +1,29 @@
+(ql:quickload "str")
+
+(defstruct (csv-struct
+            (:constructor make-csv (headers value-format-s &rest row-values)))
+  headers
+  row-values
+  value-format-s)
+
+(defun random-format-of-csv (csv)
+  "give a csv struct and generate the chaos format string of it"
+  (format t "~{~a~^,~}~%" (csv-struct-headers csv))
+  (loop for row in (csv-struct-row-values csv)
+        do (format t (csv-struct-value-format-s csv) row))
+  )
+
+(defun add-whitespaces-around (s)
+  (str:pad (+ (length s) (random 10))
+           s
+           :pad-side :center
+           :pad-char " "))
+
+(random-format-of-csv
+ (make-csv '("a" "b" "c")
+           "~{~s~a~a~}~%"
+           '("aa" 1 2) '("bb" 4 5)))
+
+(defun main ()
+  (let ((csv-headers '())
+        (csv-row ))))
