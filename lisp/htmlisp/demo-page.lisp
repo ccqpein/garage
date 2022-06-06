5e7858c8eaec86608c48f655268e0360b2c147e1
ccQpein <ccqpein@protonmail.com>
2022-06-05 23:05:21 -0400

    init

--- lisp/htmlisp/demo-page.lisp
+++ lisp/htmlisp/demo-page.lisp
@@ -3,5 +3,6 @@
 (h-body
  (h-h1 "Hello world")
  (h-p "this should has a alias tag called \"h-paragraph\"")
+ (h-paragraph "like this")
  
  )
--- lisp/htmlisp/main.lisp
+++ lisp/htmlisp/main.lisp
@@ -1,2 +1,26 @@
-(defun convert-file (file-path))
+(defun convert-file (file-path)
+  (with-open-file (f file-path :direction :input)
+    (loop
+      for obj = (read f nil nil)
+      unless obj
+        do (return all) 
+      collect obj into all)
+    ))
 
+(defun h-body-convertor (body)
+  (loop
+    for b in body
+    collect (tag-route b)))
+
+(defun h-h1-convertor (h1)
+  h1
+  )
+
+(defun h-paragraph-convertor (p)
+  p)
+
+(defun tag-route (tag-content)
+  (ccase (car tag-content)
+    (h-body (h-body-convertor (cdr tag-content)))
+    (h-h1 (h-h1-convertor (cdr tag-content)))
+    ((h-p h-paragraph) (h-paragraph-convertor (cdr tag-content)))))
