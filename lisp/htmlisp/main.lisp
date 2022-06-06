cda76b00b20d37afe47f1bdbec55f8562f7854ce
ccQpein <ccqpein@protonmail.com>
2022-06-06 00:07:11 -0400

    a bit

--- lisp/htmlisp/main.lisp
+++ lisp/htmlisp/main.lisp
@@ -7,20 +7,31 @@
       collect obj into all)
     ))
 
+(defun h-head-convertot (headers)
+  "")
+
 (defun h-body-convertor (body)
-  (loop
-    for b in body
-    collect (tag-route b)))
+  (let ((all-tags-in-body
+          (loop
+            for b in body
+            collect (tag-route b))))
+    (print all-tags-in-body)
+    (format nil "<body>~{~a~}</body>" all-tags-in-body)))
 
 (defun h-h1-convertor (h1)
-  h1
+  (format nil "<h1>~a</h1>" (car h1))
   )
 
 (defun h-paragraph-convertor (p)
-  p)
+  (format nil "<p>~a</p>" (car p)))
 
 (defun tag-route (tag-content)
   (ccase (car tag-content)
+    (h-head (h-head-convertot (cdr tag-content)))
     (h-body (h-body-convertor (cdr tag-content)))
     (h-h1 (h-h1-convertor (cdr tag-content)))
     ((h-p h-paragraph) (h-paragraph-convertor (cdr tag-content)))))
+
+(defparameter *demo* nil)
+
+(setf *demo* (convert-file "/Users/ccQ/Code/garage/lisp/htmlisp/demo-page.lisp"))
