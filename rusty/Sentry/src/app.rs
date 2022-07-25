b0c599452ff74bfef07ab2320e229848c4c1f84a
ccQpein <ccqpein@protonmail.com>
2022-07-24 22:34:15 -0400

    a bit

--- rusty/Sentry/src/app.rs
+++ rusty/Sentry/src/app.rs
@@ -1,9 +1,15 @@
 use std::path::Path;
 
+use actix_web::{dev::HttpServiceFactory, web, HttpResponse};
+
 struct Resume<'r> {
+    /// page url
     page_url: &'r str,
+
+    /// page html path in filesystem
     page_name: &'r str,
 
+    /// pdf path in filesystem
     pdf: &'r str,
 }
 
@@ -16,7 +22,17 @@ impl<'r> Resume<'r> {
         }
     }
 
+    /// read the config of resume app
     fn from_file_config(path: impl AsRef<Path>) {
         todo!()
+    }
+
+    fn into_service(self) -> impl HttpServiceFactory + 'static {
+        web::scope(self.page_url)
+            .service(web::resource("/dl").to(|| HttpResponse::Ok()))
+            .default_service(web::to(|| {
+                //:= todo: return html content
+                HttpResponse::NotFound()
+            }))
     }
 }
