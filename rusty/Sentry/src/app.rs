d1865aa1187f0ab7676a7a59259369bfb9be9b4e
ccQpein <ccqpein@protonmail.com>
2022-07-25 20:56:16 -0400

    try something new

--- rusty/Sentry/src/app.rs
+++ rusty/Sentry/src/app.rs
@@ -1,7 +1,11 @@
-use std::path::Path;
+use std::{fs::File, io::Read, path::Path};
 
-use actix_web::{dev::HttpServiceFactory, web, HttpResponse};
+use actix_web::{
+    dev::HttpServiceFactory, http::header::ContentType, web, FromRequest, Handler, HttpResponse,
+    Responder,
+};
 
+#[derive(Clone)]
 struct Resume<'r> {
     /// page url
     page_url: &'r str,
@@ -27,12 +31,44 @@ impl<'r> Resume<'r> {
         todo!()
     }
 
-    fn into_service(self) -> impl HttpServiceFactory + 'static {
-        web::scope(self.page_url)
-            .service(web::resource("/dl").to(|| HttpResponse::Ok()))
-            .default_service(web::to(|| {
-                //:= todo: return html content
-                HttpResponse::NotFound()
-            }))
+    /// read the resume html
+    fn resume(&self) -> std::io::Result<String> {
+        let mut file = File::open(self.page_name)?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+        Ok(contents)
     }
+
+    // async fn into_response(self) -> std::io::Result<String> {
+    //     // HttpResponse::Ok()
+    //     //     .content_type(ContentType::html())
+    //     //     .body(self.resume().unwrap())
+    //     todo!()
+    // }
+
+    fn into_response<F, Args>() -> F
+    where
+        F: Handler<Args>,
+        Args: FromRequest + 'static,
+        F::Output: Responder + 'static,
+    {
+        todo!()
+    }
+
+    async fn into_service(self) -> impl HttpServiceFactory + 'static {
+        //let resume = self.resume().unwarp();
+        web::scope(self.page_url.clone())
+            .service(web::resource("/dl").to(|| HttpResponse::Ok()))
+            // .default_service(web::to(|| {
+            //     HttpResponse::Ok()
+            //         .content_type(ContentType::html())
+            //         .body(resume)
+            // }))
+            //.default_service(web::to(self.into_response().await))
+            .service(web::resource("").to(Self::into_response()))
+    }
+}
+
+async fn index() -> std::io::Result<String> {
+    todo!()
 }
