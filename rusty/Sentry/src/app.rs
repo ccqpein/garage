ba53e8b05b308afd7958c72cd8c5f9df07f7ada9
ccQpein <ccqpein@protonmail.com>
2022-07-27 23:03:04 -0400

    test a bit

--- rusty/Sentry/src/app.rs
+++ rusty/Sentry/src/app.rs
@@ -10,7 +10,7 @@ use actix_web::{
 
 lazy_static! {
     static ref LAST_RESUME: Resume<'static> = Resume::default();
-    static ref RESUME_HTML: &'static str = "";
+    static ref RESUME_HTML: &'static str = "empty";
 }
 
 #[derive(Clone, Default)]
@@ -59,11 +59,76 @@ impl<'r> Resume<'r> {
     where
         T: ServiceFactory<ServiceRequest, Config = (), Error = Error, InitError = ()>,
     {
-        app.service(web::scope("/resume").service(handler))
+        app.service(
+            web::scope("/resume")
+                .service(handler_html)
+                .service(handler_dl),
+        )
     }
 }
 
 #[get("/{page_url}")]
-async fn handler(path: web::Path<(String)>) -> impl Responder {
+async fn handler_html(path: web::Path<String>) -> impl Responder {
     *RESUME_HTML
+}
+
+#[get("/{page_url}/dl")]
+async fn handler_dl(path: web::Path<String>) -> impl Responder {
+    *RESUME_HTML
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{body::MessageBody, dev::ServiceResponse, http::StatusCode, test, web::Bytes};
+
+    use super::*;
+
+    #[actix_web::test]
+    async fn test_handler_path_match() {
+        let app = test::init_service(
+            App::new().service(
+                web::scope("/resume")
+                    .service(handler_html)
+                    .service(handler_dl),
+            ),
+        )
+        .await;
+
+        let req = test::TestRequest::default()
+            .uri("/resume/urlurlu")
+            .to_request();
+
+        let res = test::call_service(&app, req).await;
+        // dbg!(res.response().body());
+        // let (_, resp) = res.into_parts();
+        // assert_eq!(
+        //     Bytes::from("empty"),
+        //     resp.into_body().try_into_bytes().unwrap()
+        // );
+        assert_eq!(res.status(), StatusCode::OK);
+
+        //////////////////
+
+        let req = test::TestRequest::default()
+            .uri("/resume/urlurlu/dl")
+            .to_request();
+        // let res = test::call_service(&app, req).await;
+        // dbg!(res.response().body());
+        // let (_, resp) = res.into_parts();
+        // assert_eq!(
+        //     Bytes::from("empty"),
+        //     resp.into_body().try_into_bytes().unwrap()
+        // );
+        assert_eq!(res.status(), StatusCode::OK);
+
+        //////////////////////
+
+        let req = test::TestRequest::default()
+            .uri("/resume/urlurlu/dlaaa")
+            .to_request();
+        let res = test::call_service(&app, req).await;
+        //dbg!(req.match_info());
+        //dbg!(req.match_pattern());
+        assert_eq!(res.status(), StatusCode::NOT_FOUND)
+    }
 }
