38535e01449ceb72dec752a2802708d36f77a39b
ccQpein <ccqpein@protonmail.com>
2022-08-11 21:05:45 -0400

    a bit

--- gopher/concurrency-http-client-call/main.go
+++ gopher/concurrency-http-client-call/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/urfave/cli/v2"
 )
+
+type resultSet struct{}
 
 func makeApp() *cli.App {
 	return &cli.App{
@@ -18,16 +23,57 @@ func makeApp() *cli.App {
 				Value:   8,
 				Usage:   "the number of how many concurrency client call",
 			},
+			&cli.IntFlag{
+				Name:    "test-time",
+				Aliases: []string{"t"},
+				Value:   10,
+				Usage:   "how long run the test",
+			},
 		},
-		Action: func(ctx *cli.Context) error {
-			makeAction(ctx.Int("concurrency-num"))
+		Action: func(c *cli.Context) error {
+			ctx, cancel := context.WithTimeout(context.Background(),
+				time.Duration(c.Int("test-time"))*time.Second)
+			defer cancel()
+
+			_ = makeCall(ctx, c.Int("concurrency-num"), c.Args().Get(0))
+			//:= TODO: printResult(result)
 			return nil
 		},
 	}
 }
 
-func makeAction(c int) {
+func makeCall(ctx context.Context, c int, url string) *resultSet {
+	// handle the test result
+	//:= TODO: go func(){}
 
+	testcaseChan := make(chan int, c)
+
+	for i := 0; i < c; i++ {
+		httpClient := http.Client{Timeout: time.Second}
+		go testFunc(ctx, &httpClient, url, testcaseChan)
+	}
+
+	return nil
+}
+
+func testFunc(ctx context.Context, client *http.Client, url string, testcaseChan chan<- int) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		default:
+			resp, err := client.Get(url)
+			if err != nil {
+				testcaseChan <- 1
+			}
+
+			if resp.StatusCode != 200 {
+				testcaseChan <- 1
+			} else {
+				testcaseChan <- 0
+			}
+		}
+	}
 }
 
 func main() {
