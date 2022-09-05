a0a9b0965138f9d6a02574b01b2b424afd640558
ccQpein <ccqpein@protonmail.com>
2022-09-04 20:08:05 -0400

    a bit

--- rusty/get-rid-of-yaml/src/lib.rs
+++ rusty/get-rid-of-yaml/src/lib.rs
@@ -1,13 +1,39 @@
-use std::io::BufRead;
+use std::io::{BufRead, Split};
 
 /// read until the colon and split the buffer on it
-fn split_on_colon<R>(reader: R, buf: &mut Vec<u8>)
+fn split_on_colon<R>(reader: R) -> Split<R>
 where
     R: BufRead,
 {
-	let length = reader.
-    match reader.read_until(b':', buf) {
-        Ok(n) if n == => todo!(),
-        Err(_) => todo!(),
+    reader.split(b':')
+}
+
+//:= TODO: need a function to check the Split return from split_on_colon
+//:=
+
+#[cfg(test)]
+mod test {
+    use std::io::BufReader;
+
+    use super::*;
+
+    #[test]
+    fn test_split_on_colon() {
+        // make sure &[u8] can be used
+        let mut reader = BufReader::new("aa:a\nbbb".as_bytes());
+        let mut buf = Vec::new();
+        assert!(reader.read_until(b'\n', &mut buf).is_ok());
+        assert_eq!(buf, "aa:a\n".as_bytes());
+        assert_eq!(
+            split_on_colon(buf.as_slice()).next().unwrap().unwrap(),
+            "aa".as_bytes()
+        );
+
+        // make string can be used
+        let s = "aaa:bbb";
+        assert_eq!(
+            split_on_colon(s.as_bytes()).next().unwrap().unwrap(),
+            "aaa".as_bytes()
+        );
     }
 }
