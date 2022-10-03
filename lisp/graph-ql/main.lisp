58c4989753ee620cb9dae90e07c3e18cfdac202d
ccQpein <ccqpein@protonmail.com>
2022-10-03 19:39:22 -0400

    a bit

--- lisp/graph-ql/main.lisp
+++ lisp/graph-ql/main.lisp
@@ -1,5 +1,24 @@
-(defun check-line-terminators (s)
-  "check line terminator \\n and \\t"
-  (declare (stream s) )
+;; (defun check-line-terminators (s)
+;;   "check line terminator \\n and \\t"
+;;   (declare (stream s) )
+  
+;;   )
+
+(deftype status ()
+  '(member :in-the-comment:
+	))
+
+(defclass scanner ()
+  ()
+  (:documentation "adam scanner class"))
+
+(defgeneric clear (scanner)
+  (:documentation "clear all the content of this scanner"))
+
+(defclass block-scanner (scanner)
+  ()
+  (:documentation "scanner for {} block"))
+
+(defun scan (rest-s status-stack)
   
   )
