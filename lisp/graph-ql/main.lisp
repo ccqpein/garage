77fdbfaf3c79ea56606540553072191b0d777c8c
ccQpein <ccqpein@protonmail.com>
2022-10-09 16:49:26 -0400

    a bit

--- lisp/graph-ql/main.lisp
+++ lisp/graph-ql/main.lisp
@@ -17,7 +17,6 @@
 
 (defclass block-scanner (scanner)
   (
-   ;;(sub-scanners :accessor sub-scanners)
    (tokens
 	:initform nil
 	:accessor tokens)
@@ -34,18 +33,30 @@
 	   (if (/= 0 (length word-token))
 		   (setf (tokens s) (append (tokens s)
 									(list (concatenate 'string (reverse word-token)))))))
-	(ccase c
+	(case c
 	  (#\{ (setf (tokens s)
 				 (append (tokens s)
 						 (list (let ((sub-block-scanner (make-instance 'block-scanner)))
 								 (scan sub-block-scanner stream)
 								 sub-block-scanner)))))
-	  (#\(  ) ;;:= todo parenthesis-scanner
+	  
+	  (#\( (setf (tokens s)
+				 (append (tokens s)
+						 (list
+						  (concatenate 'string (reverse word-token))
+						  (let ((sub-block-scanner (make-instance 'parenthesis-scanner)))
+							(scan sub-block-scanner stream)
+							sub-block-scanner)))
+				 word-token
+				 nil))
+	  
 	  ((#\  #\, #\newline #\#) ;; ignore tokens 
 	   (if (/= 0 (length word-token))
-		   (setf (tokens s) (append (tokens s)
-									(list (concatenate 'string (reverse word-token))))
-				 word-token nil)
+		   (setf (tokens s)
+				 (append (tokens s)
+						 (list (concatenate 'string (reverse word-token))))
+				 word-token
+				 nil)
 		   ))
 	  (otherwise (push c word-token))
 	  )))
@@ -57,6 +68,9 @@
   (:documentation "scanner of () block")
   )
 
+(defmethod print-object ((ps parenthesis-scanner) stream)
+  (format stream "{parenthesis block: tokens: ~{~a~^, ~}}" (tokens ps)))
+
 (defmethod scan ((s parenthesis-scanner) stream)
   (do ((c (read-char stream nil nil) (read-char stream nil nil))
 	   (word-token nil))
@@ -64,7 +78,7 @@
 	   (if (/= 0 (length word-token))
 		   (setf (tokens s) (append (tokens s)
 									(list (concatenate 'string (reverse word-token)))))))
-	(ccase c
+	(case c
 	  (#\:
 	   (if (/= 0 (length word-token))
 		   (setf (tokens s) (append (tokens s)
