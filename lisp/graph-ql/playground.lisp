4cc7e8af49cd79e3811e19d677683f15a86fc351
ccQpein <ccqpein@protonmail.com>
2022-10-16 21:21:58 -0400

    a bit

--- lisp/graph-ql/playground.lisp
+++ lisp/graph-ql/playground.lisp
@@ -75,3 +75,31 @@
   (scan bs ss)
   (format t "~a" (tokens bs))
   )
+
+
+(defvar *case5* "query HeroComparison($first: Int = 3) {
+  leftComparison: hero(episode: EMPIRE) {
+    ...comparisonFields
+  }
+  rightComparison: hero(episode: JEDI) {
+    ...comparisonFields
+  }
+}
+
+fragment comparisonFields on Character {
+  name
+  friendsConnection(first: $first) {
+    totalCount
+    edges {
+      node {
+        name
+      }
+    }
+  }
+}")
+
+(let ((bs (make-instance 'plain-scanner))
+	  (ss (make-string-input-stream *case5*)))
+  (scan bs ss)
+  (format t "~a" (tokens bs))
+  )
