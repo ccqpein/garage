66947f62b703e6ba57cdfe55be2d57854e548a98
ccQpein <ccqpein@protonmail.com>
2022-11-22 22:38:52 -0500

    a bit

---
+++ lisp/graph-ql/scanner-test.lisp
@@ -0,0 +1,13 @@
+(defpackage #:scanner-test
+  (:use #:CL #:lisp-unit)
+  
+  )
+
+(in-package #:scanner-test)
+
+(define-test block-scanner-test)
+
+(define-test parenthesis-scanner-test)
+
+(define-test plain-scanner-test)
+
