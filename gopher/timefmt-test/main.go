180c7c79694cd79c9b7a5ce7cf549b0594bd8d7c
ccQpein <ccqpein@protonmail.com>
2022-12-01 16:02:28 -0500

    a bit

--- gopher/timefmt-test/main.go
+++ gopher/timefmt-test/main.go
@@ -13,6 +13,11 @@ func main() {
 	t0, err := timefmt.Parse(now0, "%Y-%m-%dT%H:%M:%S.%fZ")
 	fmt.Println(t0, err)
 
+	// now0 = time.Now().In(time.UTC).Format(time.RFC3339)
+	// fmt.Println(now0)
+	// t0, err = timefmt.Parse(now0, "%Y-%m-%dT%H:%M:%S.%fZ")
+	// fmt.Println(t0, err)
+
 	// t1, err := timefmt.Parse("Wed Jul 28 1993 14:39:07 GMT-0400 (Eastern Daylight Time)", "%a %b %d %Y %H:%M:%S %Z%z (%Z)")
 	// fmt.Println(t1, err)
 
