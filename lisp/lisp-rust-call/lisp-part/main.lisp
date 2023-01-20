e8c7b177a994e9b2b4d6a526202d918b3d34fd9d
ccQpein <ccqpein@protonmail.com>
2023-01-20 00:01:32 -0500

    try something new

--- lisp/lisp-rust-call/lisp-part/main.lisp
+++ lisp/lisp-rust-call/lisp-part/main.lisp
@@ -5,6 +5,16 @@
 
 (cffi:use-foreign-library hello-rust)
 
+;; mod1 function
 (cffi:defcfun ("hello_mod1" hello-rust-in-mod-1) :string)
 
 (hello-rust-in-mod-1) 
+
+
+;; mod2 function
+(cffi:defcfun ("input_func_as_parameter" input-func-as-parameter-in-mod-2)
+	:void
+  (f :pointer)
+  (n :int))
+
+(input-func-as-parameter-in-mod-2 (lambda (x) (+ x 1)) 15)
--- lisp/lisp-rust-call/rust-part/src/lib.rs
+++ lisp/lisp-rust-call/rust-part/src/lib.rs
@@ -13,11 +13,13 @@ mod mod1 {
     }
 }
 
-/// add the tcp app
-mod mod2 {
+/// tokio runtime to cffi
+/*mod mod2 {
+    use async_ffi::{FfiFuture, FutureExt};
     use std::error::Error;
     use tokio::io::AsyncWriteExt;
     use tokio::net::TcpStream;
+    use tokio::runtime::Handle;
     use tokio::runtime::Runtime;
 
     #[no_mangle]
@@ -25,29 +27,63 @@ mod mod2 {
         Box::into_raw(Box::new(Runtime::new().unwrap()))
     }
 
+    // #[no_mangle]
+    // pub extern "C" fn _safe_connect(
+    //     rt_ptr: *mut Runtime,
+    //     safe_ptr: *mut Safe,
+    //     bootstrap_contact: *const c_char,
+    // ) {
+    //     assert!(!safe_ptr.is_null());
+    //     assert!(!rt_ptr.is_null());
+
+    //     let bootstrap_contact = unsafe { CStr::from_ptr(bootstrap_contact) };
+    //     let mut bootstrap_contacts = BootstrapConfig::default();
+    //     bootstrap_contacts.insert(
+    //         bootstrap_contact
+    //             .parse()
+    //             .expect("Invalid bootstrap address"),
+    //     );
+
+    //     unsafe {
+    //         let _safe = &mut *safe_ptr;
+    //         let rt = &mut *rt_ptr;
+    //         rt.block_on(_safe.connect(None, None, Some(bootstrap_contacts)))
+    //             .unwrap();
+    //     }
+    // }
+}*/
+
+mod mod2 {
+    use std::net::{TcpListener, TcpStream};
+
     #[no_mangle]
-    pub extern "C" fn _safe_connect(
-        rt_ptr: *mut Runtime,
-        safe_ptr: *mut Safe,
-        bootstrap_contact: *const c_char,
-    ) {
-        assert!(!safe_ptr.is_null());
-        assert!(!rt_ptr.is_null());
+    pub extern "C" fn input_func_as_parameter<F>(f: *const F, n: i32)
+    where
+        F: Fn(i32) -> i32,
+    {
+        // if I don't use the point, I don't know the type in common lisp
+        unsafe { println!("hello: {}", (*f)(n)) }
+    }
 
-        let bootstrap_contact = unsafe { CStr::from_ptr(bootstrap_contact) };
-        let mut bootstrap_contacts = BootstrapConfig::default();
-        bootstrap_contacts.insert(
-            bootstrap_contact
-                .parse()
-                .expect("Invalid bootstrap address"),
-        );
+    #[no_mangle]
+    pub extern "C" fn open_tcp_server<F>(handle: F) -> std::io::Result<()>
+    where
+        F: Fn(TcpStream) -> std::io::Result<()>,
+    {
+        let listener = TcpListener::bind("127.0.0.1:80")?;
 
-        unsafe {
-            let _safe = &mut *safe_ptr;
-            let rt = &mut *rt_ptr;
-            rt.block_on(_safe.connect(None, None, Some(bootstrap_contacts)))
-                .unwrap();
+        // accept connections and process them serially
+        for stream in listener.incoming() {
+            match stream {
+                Ok(ss) => {
+                    handle(ss);
+                }
+                Err(e) => {
+                    println!("err: {}", e.to_string())
+                }
+            }
         }
+        Ok(())
     }
 }
 
