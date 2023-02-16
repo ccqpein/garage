98c39082e7330b8bf483142aa468e3ebe8de07bb
ccQpein <ccqpein@protonmail.com>
2023-02-15 22:30:09 -0500

    Done loop and value parser functions

--- rusty/cl-format/src/args.rs
+++ rusty/cl-format/src/args.rs
@@ -12,24 +12,13 @@ use std::collections::HashMap;
 use std::fmt::Debug;
 use std::io::{BufRead, Cursor, Read, Seek, SeekFrom};
 
-// pub trait Tilde: Debug {
-//     fn from_buf(c: &[u8]) -> Result<Self, String>
-//     where
-//         Self: Sized;
-// }
-
-// lazy_static! {
-//     //static ref newl: Box<(dyn Tilde + Sync)> = Box::new(NewLine::new());
-//     pub static ref tilde_table: HashMap<u8, NewLine> = {
-//         let mut t = HashMap::new();
-//         t.insert(b'%', NewLine::new());
-//         t
-//     };
-// }
-
+#[derive(Debug, PartialEq)]
 enum Tilde {
     /// ~C or ~:C
     Char,
+
+    /// ~a
+    Va,
 
     /// loop
     Loop(Vec<Tilde>),
@@ -70,31 +59,36 @@ enum ErrorKind {
 }
 
 impl Tilde {
+    /// has to have '~' at current posiont of cursor
     fn parse(mut c: &mut Cursor<&'_ str>) -> Result<Self, Box<dyn std::error::Error>> {
-        let mut result = vec![];
         let mut char_buf = [0u8; 1];
-        loop {
-            c.read(&mut char_buf);
-            if char_buf[0] == b'~' {
-                c.read(&mut char_buf);
-                match char_buf[0] {
-                    b'{' => {
-                        c.seek(SeekFrom::Current(-2));
-                        result.push(Self::parse_loop(c)?)
-                    }
-                    _ => todo!(),
-                }
-            } else {
-            }
+
+        c.read(&mut char_buf)?;
+        if char_buf[0] != b'~' {
+            return Err(
+                TildeError::new(ErrorKind::ParseError, "'~' has to be the first char").into(),
+            );
         }
 
-        Ok(Self::VecTilde(result))
+        c.read(&mut char_buf)?;
+        match char_buf.as_slice() {
+            [b'a'] => {
+                c.seek(SeekFrom::Current(-2))?;
+                Self::parse_value(c)
+            }
+            [b'{'] => {
+                c.seek(SeekFrom::Current(-2))?;
+                Self::parse_loop(c)
+            }
+            _ => todo!(),
+        }
     }
 
-    fn parse_loop(mut c: &mut Cursor<&'_ str>) -> Result<Self, Box<dyn std::error::Error>> {
-        let mut char_buf = [0u8; 2];
-        c.read(&mut char_buf);
-        if let Ok(s) = std::str::from_utf8(&char_buf) && s!= "~{" {
+    fn parse_loop(c: &mut Cursor<&'_ str>) -> Result<Self, Box<dyn std::error::Error>> {
+        let mut char_buf = [0u8; 2]; // two bytes
+        c.read(&mut char_buf)?;
+        if let Ok(s) = std::str::from_utf8(&char_buf) && s != "~{" {
+			c.seek(SeekFrom::Current(-2))?; // restore the location
             return Err(
 				TildeError::new(
 					ErrorKind::ParseError,
@@ -102,19 +96,57 @@ impl Tilde {
 				).into());
         }
 
-        Ok(Self::Loop(vec![]))
+        let mut result = vec![];
+        let mut buf = vec![];
+
+        loop {
+            // read text until the next '~'
+            c.read_until(b'~', &mut buf)?;
+
+            match buf.as_slice() {
+                [b'~'] => {
+                    c.seek(SeekFrom::Current(-1))?;
+                }
+                [.., b'~'] => {
+                    c.seek(SeekFrom::Current(-1))?;
+                    result.push(Tilde::Text(String::from_utf8(
+                        buf[..buf.len() - 1].to_vec(),
+                    )?));
+                }
+                [..] => {
+                    result.push(Tilde::Text(String::from_utf8(buf.to_vec())?));
+                    return Ok(Self::Loop(result));
+                }
+            }
+
+            c.read(&mut char_buf)?;
+            if let Ok(s) = std::str::from_utf8(&char_buf) && s == "~}" {
+				return Ok(Self::Loop(result))
+			}
+
+            c.seek(SeekFrom::Current(-2))?;
+
+            // read the tilde
+            result.push(Tilde::parse(c)?);
+            buf.clear()
+        }
+    }
+
+    fn parse_value(c: &mut Cursor<&'_ str>) -> Result<Self, Box<dyn std::error::Error>> {
+        let mut char_buf = [0u8; 2]; // two bytes
+        c.read(&mut char_buf)?;
+        if let Ok(s) = std::str::from_utf8(&char_buf) && s != "~a" {
+			c.seek(SeekFrom::Current(-2))?; // restore the location
+            return Err(
+				TildeError::new(
+					ErrorKind::ParseError,
+					"should start with ~a",
+				).into());
+        }
+
+        Ok(Self::Va)
     }
 }
-
-// #[derive(Debug)]
-// enum ParseData {
-//     B(Vec<u8>),
-//     T(Box<dyn Tilde>),
-// }
-
-/*:= implenment notes:
-use cursor
- */
 
 /// the control string should including:
 /// 1. the whole string
@@ -137,111 +169,50 @@ impl<'a> ControlStr<'a> {
     fn scan(s: &'a str) -> Vec<((usize, usize), Tilde)> {
         todo!()
     }
-
-    // fn parse(&self) -> Result<Vec<ParseData>, String> {
-    //     let mut cursor = Cursor::new(self.inner);
-    //     let mut result = vec![];
-    //     loop {
-    //         match self.scan(&mut cursor)? {
-    //             Some(d) => result.push(d),
-    //             None => return Ok(result),
-    //         }
-    //     }
-    // }
-
-    //     fn scan(&self, c: &mut Cursor<&str>) -> Result<Option<ParseData>, String> {
-    //         match self.until_tilde(c)? {
-    //             a @ Some(_) => return Ok(a),
-    //             None => (), // maybe start with ~
-    //         }
-
-    //         match self.read_next_tilde(c)? {
-    //             None => Ok(None),
-    //             a @ Some(_) => return Ok(a),
-    //         }
-    //     }
-
-    //     fn until_tilde(&self, c: &mut Cursor<&str>) -> Result<Option<ParseData>, String> {
-    //         let mut buf = vec![];
-    //         let i = c.read_until(b'~', &mut buf).map_err(|e| e.to_string())?;
-
-    //         // start with '~' is other parser's job
-    //         if i == 1 {
-    //             c.seek(SeekFrom::Current(-1));
-    //             return Ok(None);
-    //         }
-
-    //         if let Some(lst) = buf.last() {
-    //             if *lst == b'~' {
-    //                 buf.pop();
-    //                 c.seek(SeekFrom::Current(-1));
-    //             } // else means it is the endding
-    //         } else {
-    //             return Ok(None);
-    //         }
-
-    //         Ok(Some(ParseData::B(buf)))
-    //     }
-
-    //     //:= TODO: just demo now
-    //     fn read_next_tilde(&self, c: &mut Cursor<&str>) -> Result<Option<ParseData>, String> {
-    //         let mut buffer = vec![];
-    //         let mut one_byte_buffer: [u8; 1] = [0; 1];
-    //         let mut i = 0;
-
-    //         let mut count = 0;
-    //         loop {
-    //             i = c.read(&mut one_byte_buffer).map_err(|e| e.to_string())?;
-    //             if i == 0 {
-    //                 break;
-    //             }
-    //             //:= TODO: more rule here
-    //             count += 1;
-    //             buffer.extend_from_slice(&one_byte_buffer);
-    //             if count == 2 {
-    //                 break;
-    //             }
-    //         }
-
-    //         //:= TODO: parse to real data type here
-    //         if buffer.len() == 0 {
-    //             return Ok(None);
-    //         }
-    //         return Ok(Some(ParseData::B(buffer)));
-    //     }
 }
 
-// #[cfg(test)]
-// mod test {
-//     use super::*;
+#[cfg(test)]
+mod test {
+    use super::*;
 
-//     #[test]
-//     fn test_cursor() {
-//         let mut testcase0 = Cursor::new("abcd");
-//         assert_eq!(testcase0.position(), 0);
+    #[test]
+    fn test_cursor() {
+        let mut testcase0 = Cursor::new("abcd");
+        assert_eq!(testcase0.position(), 0);
 
-//         let mut buffer: [u8; 1] = [0; 1];
-//         testcase0.set_position(2);
-//         let _ = testcase0.read(&mut buffer);
-//         assert_eq!(buffer[0], b'c');
-//     }
+        let mut buffer: [u8; 1] = [0; 1];
+        testcase0.set_position(2);
+        let _ = testcase0.read(&mut buffer);
+        assert_eq!(buffer[0], b'c');
+    }
 
-//     //:= TODO: demo test
-//     #[test]
-//     fn test_parse() {
-//         let testcase0 = ControlStr::new("aaa ~D~%");
-//         dbg!(testcase0
-//             .parse()
-//             .unwrap()
-//             .iter()
-//             .map(|v| match v {
-//                 ParseData::B(b) => {
-//                     String::from_utf8(b.clone()).unwrap()
-//                 }
-//                 _ => {
-//                     String::new()
-//                 }
-//             })
-//             .collect::<Vec<String>>());
-//     }
-// }
+    #[test]
+    fn test_parse_va() -> Result<(), Box<dyn std::error::Error>> {
+        let mut case = Cursor::new("~a");
+        assert_eq!(Tilde::parse_value(&mut case)?, Tilde::Va,);
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_loop() -> Result<(), Box<dyn std::error::Error>> {
+        let mut case = Cursor::new("~{~}");
+
+        assert_eq!(Tilde::parse_loop(&mut case)?, Tilde::Loop(Vec::new()));
+
+        let mut case = Cursor::new("~{a bc~}");
+
+        assert_eq!(
+            Tilde::parse_loop(&mut case)?,
+            Tilde::Loop(vec![Tilde::Text(String::from("a bc"))])
+        );
+
+        let mut case = Cursor::new("~{a bc~a~}");
+
+        assert_eq!(
+            Tilde::parse_loop(&mut case)?,
+            Tilde::Loop(vec![Tilde::Text(String::from("a bc")), Tilde::Va,])
+        );
+
+        Ok(())
+    }
+}
