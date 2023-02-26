3b0b39791722984d0207255f2c833778ca7cd7b6
ccQpein <ccqpein@protonmail.com>
2023-02-26 15:13:56 -0500

    done for now

--- rusty/cl-format-macros/src/lib.rs
+++ rusty/cl-format-macros/src/lib.rs
@@ -10,6 +10,7 @@ pub enum TildeKind {
     Char,
 
     /// ~$ ~5$ ~f
+    #[implTo(float)]
     Float(Option<String>),
 
     /// ~d ~:d ~:@d
@@ -74,31 +75,74 @@ trait TildeKindVa {}
 ```
 "]
 
-use std::error::Error;
+use std::{collections::HashMap, error::Error};
 
 use proc_macro::TokenStream;
-use proc_macro2::{Ident, Punct, Span};
+use proc_macro2::{Ident, Span};
 use quote::quote;
-use syn::{
-    parse_macro_input, parse_quote, Attribute, Data, DataEnum, DataStruct, DeriveInput, Fields,
-    GenericParam, Generics, Index, Variant,
-};
+use syn::{parse_macro_input, Attribute, Data, DataEnum, DeriveInput, Variant};
 
 #[proc_macro_derive(TildeAble, attributes(implTo))]
 pub fn derive_tilde_able(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
+
+    //let mut all_methods_headers = vec![];
+    let mut return_types_traits = vec![];
+    let mut all_default_methods = vec![];
+    let mut types_impl_methods = HashMap::new();
+
     match input.data {
         Data::Enum(DataEnum { ref variants, .. }) => {
-            //variants.iter().filter_map(|v| v.attrs.)
+            let all_vars = variants.iter().map(|var| parse_variant_attrs(var));
+
+            all_vars.for_each(|(field, tys)| {
+                let fname = Ident::new(
+                    &(String::from("into_tildekind_") + &field.to_lowercase()),
+                    Span::call_site(),
+                );
+
+                let return_type =
+                    Ident::new(&(String::from("TildeKind") + &field), Span::call_site());
+
+                // add default methods to TildeAble
+                all_default_methods
+                    .push(quote! {fn #fname(&self) -> Option<&dyn #return_type> {None}});
+
+                //
+                tys.for_each(|ty| {
+                    let en = types_impl_methods.entry(ty).or_insert(vec![]);
+                    en.push(quote! {fn #fname(&self) -> Option<&dyn #return_type> {Some(&self)}})
+                });
+
+                //
+                return_types_traits.push(quote! {trait #return_type {}})
+            });
         }
         _ => panic!("only support the enum"),
-    }
+    };
 
-    proc_macro2::TokenStream::new().into()
+    let mut result = vec![];
+
+    // trait TildeAble defination
+    let tilde_able_trait = quote! {trait TildeAble {#(#all_default_methods)*}};
+
+    let mut auto_impl_for_types = types_impl_methods
+        .iter()
+        .map(|(ty, methods)| {
+            quote! {impl TildeAble for #ty {#(#methods)*}}
+        })
+        .collect();
+
+    // merge together
+    result.push(tilde_able_trait);
+    result.append(&mut auto_impl_for_types);
+    result.append(&mut return_types_traits);
+
+    proc_macro2::TokenStream::from_iter(result.into_iter()).into()
 }
 
 /// return the field Ident and all types implTo if it has implTo
-fn parse_variant_attrs(variant: &Variant) -> (Ident, impl Iterator<Item = Ident> + '_) {
+fn parse_variant_attrs(variant: &Variant) -> (String, impl Iterator<Item = Ident> + '_) {
     let all_impl_to_type = variant
         .attrs
         .iter()
@@ -106,7 +150,7 @@ fn parse_variant_attrs(variant: &Variant) -> (Ident, impl Iterator<Item = Ident>
         .map(|attr| get_types_impl_to(attr).unwrap())
         .flatten();
 
-    let field = &variant.ident;
+    let field = variant.ident.to_string();
 
     (field.clone(), all_impl_to_type)
 }
@@ -175,7 +219,7 @@ mod test {
 
         //dbg!(test_case);
         let result = parse_variant_attrs(&test_case);
-        assert_eq!(result.0, Ident::new("A", Span::call_site()));
+        assert_eq!(result.0, "A");
         assert_eq!(
             result.1.map(|i| i.to_string()).collect::<Vec<_>>(),
             vec!["a", "b", "c", "d"]
@@ -184,13 +228,14 @@ mod test {
                 .collect::<Vec<String>>(),
         );
 
+        //
         let test_case: Variant = parse_quote! {
             B
         };
 
         //dbg!(&test_case);
         let mut result = parse_variant_attrs(&test_case);
-        assert_eq!(result.0, Ident::new("B", Span::call_site()));
+        assert_eq!(result.0, "B");
         assert_eq!(result.1.next(), None);
 
         Ok(())
