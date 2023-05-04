f42a8ac43af2fd2714a6d017f357f0e949e08bdf
ccQpein <ccqpein@protonmail.com>
2023-05-04 20:24:17 +0800

    a bit

--- rusty/test-trait-type-binding-dispatch/src/main.rs
+++ rusty/test-trait-type-binding-dispatch/src/main.rs
@@ -26,4 +26,42 @@ impl AppInput<AppBInput> for AppB {
     }
 }
 
-fn main() {}
+//
+// test the struct dyn to diff types
+//
+
+trait B0 {
+    fn output(&self) -> String {
+        String::from("B0")
+    }
+}
+
+trait B1 {
+    fn output(&self) -> String {
+        String::from("B1")
+    }
+}
+
+trait B2 {
+    fn output(&self) -> String {
+        String::from("B2")
+    }
+}
+
+impl B0 for AppB {
+    fn output(&self) -> String {
+        String::from("B0")
+    }
+}
+
+impl B1 for AppB {
+    fn output(&self) -> String {
+        String::from("B1")
+    }
+}
+
+fn main() {
+    let b = AppB {};
+    println!("{}", <AppB as B0>::output(&b));
+    println!("{}", <AppB as B1>::output(&b));
+}
