cc6b7b47fa16ecf85e09bac0e07bec576866f099
ccQpein <ccqpein@protonmail.com>
2023-06-29 22:19:44 -0400

    new demo

---
+++ rusty/rss-demo/src/main.rs
@@ -0,0 +1,16 @@
+use rss::Channel;
+use std::error::Error;
+
+async fn example_feed() -> Result<Channel, Box<dyn Error>> {
+    let content = reqwest::get("http://example.com/feed.xml")
+        .await?
+        .bytes()
+        .await?;
+    let channel = Channel::read_from(&content[..])?;
+    Ok(channel)
+}
+
+#[tokio::main]
+fn main() {
+    println!("Hello, world!");
+}
