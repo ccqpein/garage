0d2a2e26dee0ba308723fc30a2bd4e88c4285d72
ccQpein <ccqpein@protonmail.com>
2023-07-01 21:44:35 -0400

    a bit

--- rusty/leptos-tutorial/src/main.rs
+++ rusty/leptos-tutorial/src/main.rs
@@ -1,5 +1,54 @@
 use leptos::*;
 
 fn main() {
-    mount_to_body(|cx| view! { cx,  <p>"Hello, world?"</p> })
+    //mount_to_body(|cx| view! { cx,  <p>"Hello, world?"</p> })
+    mount_to_body(|cx| view! { cx,  <App/> })
+}
+
+#[component]
+fn App(cx: Scope) -> impl IntoView {
+    let (count, set_count) = create_signal(cx, 0);
+
+    let double_count = move || count() * 2;
+
+    view! { cx,
+        <button
+            on:click=move |_| {
+                set_count.update(|n| *n += 1);
+            }
+            // the class: syntax reactively updates a single class
+            // here, we'll set the `red` class when `count` is odd
+            class:red=move || count() % 2 == 1
+        >
+            "Click me"
+        </button>
+        // NOTE: self-closing tags like <br> need an explicit /
+        <br/>
+
+        // We'll update this progress bar every time `count` changes
+        <progress
+            // static attributes work as in HTML
+            max="50"
+
+            // passing a function to an attribute
+            // reactively sets that attribute
+            // signals are functions, so this <=> `move || count.get()`
+            value=count
+        >
+        </progress>
+        <br/>
+
+        // This progress bar will use `double_count`
+        // so it should move twice as fast!
+        <progress
+            max="50"
+            // derived signals are functions, so they can also
+            // reactive update the DOM
+            value=double_count
+        >
+        </progress>
+            //<p>"Count: " {count()}</p> // inreactive
+            <p>"Count: " {count}</p>
+        <p>"Double Count: " {double_count}</p>
+    }
 }
