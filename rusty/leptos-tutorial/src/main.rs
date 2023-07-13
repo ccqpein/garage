9db1808e4b2815689d2143bae319340d2802ca6c
ccQpein <ccqpein@protonmail.com>
2023-07-12 22:32:30 -0400

    keep learning

--- rusty/leptos-tutorial/src/main.rs
+++ rusty/leptos-tutorial/src/main.rs
@@ -663,3 +663,24 @@ fn main() {
 // }
 
 //==========================================================
+
+#[component]
+pub fn App(cx: Scope) -> impl IntoView {
+    // a signal holds a value, and can be updated
+    let (count, set_count) = create_signal(cx, 0);
+
+    // a derived signal is a function that accesses other signals
+    let double_count = move || count() * 2;
+    let count_is_odd = move || count() & 1 == 1;
+    let text = move || if count_is_odd() { "odd" } else { "even" };
+
+    // an effect automatically tracks the signals it depends on
+    // and reruns when they change
+    create_effect(cx, move |_| {
+        log!("text = {}", text());
+    });
+
+    view! { cx,
+        <p>{move || text().to_uppercase()}</p>
+    }
+}
