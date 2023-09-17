f027c52219def653b61ae167ffd271e669e64f20
ccQpein <ccqpein@protonmail.com>
2023-09-17 17:49:14 -0400

    emacs receipt updated

--- lisp/espresso/receipts/emacs.lisp
+++ lisp/espresso/receipts/emacs.lisp
@@ -15,30 +15,68 @@
   "receipts of this pakcage")
 
 ;; cache folder
-(defparameter *download-folder* "~/Downloads")
+(defparameter *download-folder* (pathname "~/Downloads/build-emacs-for-macos/"))
 
-(defun download-and-build-souce-code ()
-  (let ((download-path (pathname (format nil
-										 "~a/build-emacs-for-macos"
-										 *download-folder*))))
-	(format t "download-path: ~a~%" download-path)
-	(unless (if-file-exist download-path)
-	  (shell-run-program
-	   (format nil
-			   "git clone https://github.com/jimeh/build-emacs-for-macos.git ~a"
-			   download-path)
-	   :output *receipts-output*
-	   :error-output *receipts-error*))
+;; (defun download-and-build-souce-code ()
+;;   (let ((download-path (pathname (format nil
+;; 										 "~a/build-emacs-for-macos"
+;; 										 *download-folder*))))
+;; 	(format t "download-path: ~a~%" download-path)
+;; 	(unless (if-file-exist download-path)
+;; 	  (shell-run-program
+;; 	   (format nil
+;; 			   "git clone https://github.com/jimeh/build-emacs-for-macos.git ~a"
+;; 			   download-path)
+;; 	   :output *receipts-output*
+;; 	   :error-output *receipts-error*))
 
-	(uiop:with-current-directory (download-path)
-	  (format t "jump inside ~a~%" download-path)
-	  (shell-run-program "./build-emacs-for-macos"
-				   :output *receipts-output*
-				   :error-output *receipts-error*)
-	  )))
+;; 	(uiop:with-current-directory (download-path)
+;; 	  (format t "jump inside ~a~%" download-path)
+;; 	  (shell-run-program "./build-emacs-for-macos"
+;; 				   :output *receipts-output*
+;; 				   :error-output *receipts-error*)
+;; 	  )))
+
+(defun download-souce-code ()
+  (format t "download-path: ~a~%" *download-folder*)
+  (unless (if-file-exist *download-folder*)
+	(shell-run-program
+	 (format nil
+			 "git clone https://github.com/jimeh/build-emacs-for-macos.git ~a"
+			 *download-folder*)
+	 :output *receipts-output*
+	 :error-output *receipts-error*)))
+
+(defun install (&rest rest)
+  (declare (ignore rest))
+  (download-souce-code)
+  (uiop:with-current-directory (*download-folder*)
+	(shell-run-program "./build-emacs-for-macos"
+					   :output *receipts-output*
+					   :error-output *receipts-error*)
+	))
+
+(defun update (&rest rest)
+  (declare (ignore rest))
+  (uiop:with-current-directory (*download-folder*)
+	(shell-run-program
+	 "git pull"
+	 :output *receipts-output*
+	 :error-output *receipts-error*)
+	))
+
+(defun upgrade (&rest rest)
+  (declare (ignore rest))
+  (uiop:with-current-directory (*download-folder*)
+	(shell-run-program "./build-emacs-for-macos"
+					   :output *receipts-output*
+					   :error-output *receipts-error*)
+	))
 
 (push (make-instance 'standard-receipt
 					 :receipt-version "master"
-					 :install-func #'download-and-build-souce-code)
+					 :install-func #'install
+					 :update-func #'update
+					 :upgrade-func #'upgrade)
 	  *receipts*)
 
