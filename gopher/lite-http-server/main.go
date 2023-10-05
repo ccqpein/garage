9be51d9dce498b16d3e1915e84937c35d198a391
ccQpein <ccqpein@protonmail.com>
2023-10-04 21:29:17 -0400

    a bit

--- gopher/lite-http-server/main.go
+++ gopher/lite-http-server/main.go
@@ -14,9 +14,27 @@ func index(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fs := http.FileServer(http.Dir("../../rusty/lepto-life-game/dist/"))
 	fmt.Printf("%+v\n", fs)
-	http.Handle("/", fs)
 	http.Handle("/a/", http.StripPrefix("/a/", fs))
+	http.Handle("/", fs)
+	//http.Handle("/a/", http.StripPrefix("/a/", fs))
 
 	http.ListenAndServe(":8081", nil)
 
 }
+
+// func main() {
+// 	fileServer := http.Dir("../../rusty/lepto-life-game/dist/")
+// 	http.HandleFunc("/a/", mimeTypeHandler(fileServer))
+// 	http.HandleFunc("/", mimeTypeHandler(fileServer))
+
+// 	http.ListenAndServe(":8081", nil)
+// }
+
+// func mimeTypeHandler(fs http.FileSystem) func(w http.ResponseWriter, r *http.Request) {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		if strings.HasSuffix(r.URL.Path, ".wasm") {
+// 			w.Header().Set("content-type", "application/wasm")
+// 		}
+// 		http.FileServer(fs).ServeHTTP(w, r)
+// 	}
+// }
