60da8977ec3c9791fbc23056b60ae7ef5db5d6f5
ccQpein <ccqpein@protonmail.com>
2023-11-12 00:06:50 -0500

    a bit

---
+++ python/open-ai-chat/chat.py
@@ -0,0 +1,22 @@
+from typing import List
+from openai import OpenAI
+
+client = OpenAI()
+MODEL = "gpt-4-1106-preview"
+
+def chat_completions(cl: List):
+    '''cl example: [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Who won the world series in 2020?"},
+    {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+    {"role": "user", "content": "Where was it played?"}
+    ]'''
+
+    response = client.chat.completions.create(
+        model=MODEL,
+        messages=cl
+    )
+
+    # Choice: https://github.com/openai/openai-python/blob/e661da69b4a11d48edfe21d2b12f53c201593596/src/openai/types/chat/chat_completion.py
+    return response.choices[0].message.content
+
--- python/open-ai-chat/main.py
+++ python/open-ai-chat/main.py
@@ -1,11 +1,45 @@
-import os
-import openai
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from chat import chat_completions
+import json
 
-# Load your API key from an environment variable or secret management service
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# response = openai.Completion.create(
-#     model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        if self.path == '/chat':
+            self.chat_handle()
+        # self.send_response(200)
+        # self.send_header('Content-type', 'text/plain')
+        # self.end_headers()
+        # self.wfile.write(b"hello")
 
-response = openai.Completion.create(
-    model="text-ada-001", prompt="Say this is a test", temperature=0, max_tokens=7)
+    def chat_handle(self):
+        try:
+            content_length = int(self.headers['Content-Length'])
+            # Read the data
+            post_data = self.rfile.read(content_length)
+            data = json.loads(post_data)
+
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+
+            # just assume the data is follow the data struct
+            response = chat_completions(data)
+            self.wfile.write(response.encode())
+
+        except json.JSONDecodeError:
+            self.send_error(400, "Invalid JSON")
+
+
+def run(server_class=HTTPServer,
+        handler_class=SimpleHTTPRequestHandler,
+        addr="127.0.0.1", port=8080):
+
+    server_address = (addr, port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Starting httpd server on {addr}:{port}")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
