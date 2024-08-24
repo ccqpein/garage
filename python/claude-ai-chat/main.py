11483177748fe8015bf12370c8648bda9cf199f6
ccQpein <ccqpein@protonmail.com>
2024-08-24 14:13:23 -0400

    re-try it

--- python/claude-ai-chat/main.py
+++ python/claude-ai-chat/main.py
@@ -5,16 +5,16 @@ client = anthropic.Anthropic(
     # defaults to os.environ.get("ANTHROPIC_API_KEY")
     api_key=os.environ.get("ANTHROPIC_API_KEY"), )
 
-message = client.messages.create(model="claude-3-opus-20240229",
-                                 max_tokens=1000,
-                                 temperature=0.0,
-                                 system="Respond only in Yoda-speak.",
-                                 messages=[{
-                                     "role": "user",
-                                     "content": "How are you today?"
-                                 }])
+message = client.messages.create(
+    model="claude-3-5-sonnet-20240620",
+    max_tokens=1000,
+    # system="Respond only in Yoda-speak.",
+    messages=[{
+        "role": "user",
+        "content": "How are you today?"
+    }])
 
-print(message.content)
+print(message.content[0].model_dump()["text"])
 
 # with client.messages.stream(
 #         max_tokens=1024,
