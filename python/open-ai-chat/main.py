bb82f8acb895c4d62cac8b7c8db48dde2d0c49a5
ccQpein <ccqpein@protonmail.com>
2024-09-11 00:08:47 -0400

    a bit

--- python/open-ai-chat/main.py
+++ python/open-ai-chat/main.py
@@ -1,35 +1,36 @@
+import json
+import logging
+import sys
 from http.server import BaseHTTPRequestHandler, HTTPServer
+
+from openai import OpenAI
+
 from chat import chat_completions
 from dallE import image_generate_dalle3
 from funcall import reminder_example
-import json
-from openai import OpenAI
-import logging
-import sys
 
-#logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+# logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 
 CLIENT = OpenAI()
 FUNCS = [reminder_example()]
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header("Content-type", "text/html")
         self.end_headers()
         message = "Hello, World!"
         self.wfile.write(bytes(message, "utf8"))
         return
 
     def do_POST(self):
-        if self.path == '/chat':
+        if self.path == "/chat":
             self.chat_handle()
-        elif self.path == '/image_DALLE3':
+        elif self.path == "/image_DALLE3":
             self.DALLE3_handle()
         return
 
@@ -37,7 +38,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def chat_handle(self):
         data = {}
         try:
-            content_length = int(self.headers['Content-Length'])
+            content_length = int(self.headers["Content-Length"])
             # Read the data
             post_data = self.rfile.read(content_length)
             data = json.loads(post_data)
@@ -49,20 +50,20 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         try:
             response = chat_completions(CLIENT, data, False, FUNCS)
             self.send_response(200)
-            self.send_header('Content-type', 'application/json')
+            self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(response.encode())
         except Exception as e:
             self.send_response(500)  # Internal Server Error
-            self.send_header('Content-type', 'text/plain')
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(f'{e}'.encode())
+            self.wfile.write(f"{e}".encode())
         return
 
     def DALLE3_handle(self):
         data = {}
         try:
-            content_length = int(self.headers['Content-Length'])
+            content_length = int(self.headers["Content-Length"])
             # Read the data
             post_data = self.rfile.read(content_length)
             data = json.loads(post_data)
@@ -73,14 +74,14 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         try:
             response = image_generate_dalle3(CLIENT, data)
             self.send_response(200)
-            self.send_header('Content-type', 'application/json')
+            self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(response.encode())
         except Exception as e:
             self.send_response(500)  # Internal Server Error
-            self.send_header('Content-type', 'text/plain')
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(f'{e}'.encode())
+            self.wfile.write(f"{e}".encode())
         return
 
     def func_call_handle(self):
@@ -88,10 +89,11 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         return
 
 
-def run(server_class=HTTPServer,
-        handler_class=SimpleHTTPRequestHandler,
-        addr="127.0.0.1",
-        port=8080):
+    server_class=HTTPServer,
+    handler_class=SimpleHTTPRequestHandler,
+    addr="127.0.0.1",
+    port=8080,
+):
     server_address = (addr, port)
     logging.debug("Starting server setup")
 
