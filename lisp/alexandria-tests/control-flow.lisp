7e449bc3c416ba8ca48400b6b7af00f47df25ee5
ccQpein <ccqpein@protonmail.com>
2025-01-10 23:03:46 -0500

    xor tests

--- lisp/alexandria-tests/control-flow.lisp
+++ lisp/alexandria-tests/control-flow.lisp
@@ -5,14 +5,24 @@
     (values 0 nil 2)
     (values 0 1 3)))
 
-(pprint (macroexpand-1 '(alexandria:nth-value-or 1
-                         (values 0 nil 2)
-                         (values 0 1 2))))
+(pprint
+ (macroexpand-1
+  '(alexandria:nth-value-or 1
+    (values 0 nil 2)
+    (values 0 1 2))))
 
 
 (defun xor-test0 ()
-  (alexandria:xor 1 2))
+  (assert (equal '(nil nil) (multiple-value-list (alexandria:xor 1 2))))
+  (assert (equal '(2 t) (multiple-value-list (alexandria:xor nil 2))))
+  (assert (equal '(1 t) (multiple-value-list (alexandria:xor 1 nil nil))))
+  (assert (equal '(nil nil) (multiple-value-list (alexandria:xor 1 2 3))))
+  (assert (equal '(3 t) (multiple-value-list (alexandria:xor nil nil 3 nil))))
+  (assert (equal '(nil nil) (multiple-value-list (alexandria:xor nil nil 3 4))))
+  (assert (equal '(nil nil) (multiple-value-list (alexandria:xor nil 2 3 4)))))
 
 (pprint
  (macroexpand-1
   '(alexandria:xor 1 2 3)))
+
+(defun switch ())
