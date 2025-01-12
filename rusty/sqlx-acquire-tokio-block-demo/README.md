396695917ae2a5cb13afdda8afd76811560b3c76
ccQpein <ccqpein@protonmail.com>
2025-01-12 14:37:04 -0500

    more code

--- rusty/sqlx-acquire-tokio-block-demo/README.md
+++ rusty/sqlx-acquire-tokio-block-demo/README.md
@@ -7,3 +7,5 @@ run test db
 ```
 docker-compose up -d
 ```
+
+https://users.rust-lang.org/t/sqlx-connection-pool-timeout-issue-with-tokio/123892
--- rusty/sqlx-acquire-tokio-block-demo/src/main.rs
+++ rusty/sqlx-acquire-tokio-block-demo/src/main.rs
@@ -24,19 +24,44 @@ mod tests {
     // });
 
     lazy_static! {
+        //static ref rt: Runtime = Runtime::new().unwrap();
         static ref DB_CONNECTION: DatabaseConnection = {
             dbg!("in db connecting");
-            let re = Runtime::new().unwrap().block_on(async {
-                Database::connect("postgres://test_user:test_password@localhost:5432/test_db")
+            Runtime::new().unwrap().block_on(async {
+                let db = Database::connect("postgres://test_user:test_password@localhost:5432/test_db")
                     .await
-                    .expect("db connect error")
-            });
-            dbg!("connected");
-            re
+                    .expect("db connect error");
+                dbg!("connected");
+                return db
+            })
+            // let re = rt.block_on(async {
+            //     Database::connect("postgres://test_user:test_password@localhost:5432/test_db")
+            //         .await
+            //         .expect("db connect error")
+            // });
+            //dbg!("connected");
+            //re
         };
     }
 
-    fn use_connection(db: &DatabaseConnection) {}
+    fn use_connection(db: &DatabaseConnection) {
+        dbg!(db.get_postgres_connection_pool());
+        // let runtime = tokio::runtime::Runtime::new().unwrap();
+        // let result = runtime.block_on(async {
+        //     tokio::time::timeout(std::time::Duration::from_secs(1), async {
+        //         db.execute(Statement::from_string(
+        //             DatabaseBackend::Postgres,
+        //             "SELECT 1",
+        //         ))
+        //         .await
+        //     })
+        //     .await
+        // });
+
+        // if let Err(e) = result {
+        //     println!("Runtime or connection is not functional: {e}");
+        // }
+    }
 
     #[test]
     fn test() {
@@ -60,6 +85,7 @@ mod tests {
 
         let rt = Runtime::new().unwrap();
         use_connection(&DB_CONNECTION);
+        //dbg!("after use_connection");
 
         // code below is ok
         // let DB_CONNECTION = rt.block_on(async {
@@ -70,13 +96,22 @@ mod tests {
 
         rt.block_on(async {
             dbg!("in here?");
+            //tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
             let re = DB_CONNECTION
                 .execute(Statement::from_string(
                     DatabaseBackend::Postgres,
                     "DELETE FROM test_table;",
                 ))
-                .await
-                .unwrap();
+                .await;
+            dbg!(re);
+
+            let re = DB_CONNECTION
+                .execute(Statement::from_string(
+                    DatabaseBackend::Postgres,
+                    "DELETE FROM test_table;",
+                ))
+                .await;
+
             dbg!(re);
             dbg!("done");
         })
