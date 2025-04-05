df76d68b9d5e6f45df91819c88fb33c67a1ce7a5
ccQpein <ccqpein@protonmail.com>
2025-04-05 13:17:53 -0400

    still don't know how to do it

--- gopher/ristretto-with-generic-demo/main.go
+++ gopher/ristretto-with-generic-demo/main.go
@@ -2,21 +2,53 @@ package main
 
 import "github.com/dgraph-io/ristretto/v2"
 
-type A struct{}
-type B struct{}
-
-type Cache struct {
+type typeCache[K ristretto.Key] struct {
 }
 
-type keyAble interface{}
+type Cache[K ristretto.Key] struct {
+	typeTable map[string]*ristretto.Cache[K]
+}
+
+type keyAble interface {
+	ristretto.Key
+}
 
 type cacheAble[K ristretto.Key] interface {
 	acceptAbleKey() K
+	cacheTypeName() string
 }
 
-func GetCache[K ristretto.Key](c *Cache, k K, v cacheAble[K]) {
+func InsertTypeCache[K ristretto.Key, V cacheAble[K]](v V) {
+	cache, err := ristretto.NewCache(&ristretto.Config[K, V]{
+		NumCounters: 1e7,     // number of keys to track frequency of (10M).
+		MaxCost:     1 << 30, // maximum cost of cache (1GB).
+		BufferItems: 64,      // number of keys per Get buffer.
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer cache.Close()
+}
+
+func GetCache[K ristretto.Key, V cacheAble[K]](c *Cache, k K, v V) {
 
 }
+
+// /////////////////////
+type A struct{}
+type B struct{}
+
+func (a *A) acceptAbleKey() string {
+	return "a"
+}
+
+func (a *A) cacheTypeName() string {
+	return "a"
+}
+
+func Test[K ristretto.Key](v cacheAble[K]) {}
+
+///////////////////////
 
 func main() {
 	cache, err := ristretto.NewCache(&ristretto.Config[string, string]{
@@ -27,6 +59,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	defer cache.Close()
 
+	Test(&A{})
 }
