3c4f2b0f879af31d7973e22584b7e301c479121e
ccQpein <ccqpein@protonmail.com>
2025-05-22 00:51:45 -0400

    recall fn is the primary type

--- rusty/tiny-lisp-interpreter/src/expression.rs
+++ rusty/tiny-lisp-interpreter/src/expression.rs
@@ -2,20 +2,24 @@ use std::collections::HashMap;
 
 use crate::parser::*;
 
-enum Types {
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum Types {
     Number(i64),
     String(String),
     Func(String),
 }
 
 /// the arg of function call
-struct Value {
-    ty: Types,
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Value {
+    pub ty: Types,
 }
 
+type Lmda = for<'a> fn(&'a mut Context, Value) -> Value;
+
 /// Expr context when we running expression
-struct Context {
-    //fnTable: HashMap<>,
+pub struct Context {
+    pub fnTable: HashMap<String, Lmda>,
 }
 
 struct Expr<'a, 'c: 'a> {
@@ -33,9 +37,32 @@ impl<'a, 'c: 'a> Expr<'a, 'c> {
 
     fn eval_sym(&self, s: &Sym) {
         match s.vt {
-            ValueType::Number(a) => todo!(),
+            ValueType::Number(_a) => todo!(),
             ValueType::String(_) => todo!(),
             ValueType::Symbol(_) => todo!(),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::functions;
+
+    use super::*;
+
+    #[test]
+    fn test_context() {
+        let mut ctx = Context {
+            fnTable: HashMap::new(),
+        };
+
+        ctx.fnTable.insert("add".to_string(), functions::math::add);
+
+        dbg!(ctx.fnTable.get("add").unwrap()(
+            &mut ctx,
+            Value {
+                ty: Types::Number(1),
+            },
+        ));
     }
 }
--- rusty/tiny-lisp-interpreter/src/functions/math.rs
+++ rusty/tiny-lisp-interpreter/src/functions/math.rs
@@ -1,3 +1,9 @@
-fn add() -> i64 {
-    todo!()
+use crate::expression::*;
+
+pub fn add(ctx: &mut Context, v: Value) -> Value {
+    dbg!("hello");
+
+    Value {
+        ty: Types::Number(2),
+    }
 }
