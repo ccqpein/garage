7b1c0db12804f26988ee6306a67402a1e8772292
ccQpein <ccqpein@protonmail.com>
2025-05-26 14:20:04 -0400

    place holder for future? maybe

--- rusty/tiny-lisp-interpreter/src/functions.rs
+++ rusty/tiny-lisp-interpreter/src/functions.rs
@@ -1,6 +1,12 @@
 use crate::expression::*;
 pub mod math;
 
+pub fn lambda(ctx: &mut Context, a: &Args) -> Result<Value, ExpressionError> {
+    todo!()
+}
+
 ///:= need to make this,...
 ///:= should return the Value::Func
-pub fn lambda() {}
+pub fn defun(ctx: &mut Context, a: &Args) -> Result<Value, ExpressionError> {
+    todo!()
+}
