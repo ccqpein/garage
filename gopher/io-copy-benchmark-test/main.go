27e5631ca4de9eb796705ad7373bced4d8c867ba
ccQpein <ccqpein@protonmail.com>
2025-05-29 20:44:40 -0400

    a bit

--- gopher/io-copy-benchmark-test/main.go
+++ gopher/io-copy-benchmark-test/main.go
@@ -1,3 +1,25 @@
 package main
 
+import (
+	"io"
+	"os"
+)
+
+func aa() {
+	destinationFile, errCreate := os.Create("~/Desktop/aaa")
+	if errCreate != nil {
+		panic(errCreate)
+	}
+
+	sourceFile, errOpen := os.Open("~/Desktop/bbb")
+	if errOpen != nil {
+		panic(errOpen)
+	}
+
+	_, errCopy := io.Copy(destinationFile, sourceFile) // Uses internal 32KB buffer
+	if errCopy != nil {
+		panic(errCopy)
+	}
+}
+
 func main() {}
