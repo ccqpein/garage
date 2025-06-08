9044c6e0d3b75d5b3f65a0eb5ee1d2492139df00
ccQpein <ccqpein@protonmail.com>
2025-06-08 14:59:59 -0400

    keep doing

---
+++ ai/hyper-mcp-test/llama-agent-demo/main.py
@@ -0,0 +1,61 @@
+import asyncio
+import json
+
+from fastmcp import Client
+
+
+async def get_tools_des():
+    async with Client("http://127.0.0.1:3001/sse") as client:
+        tools = await client.list_tools()
+        return tools
+
+
+TOOLS = asyncio.run(get_tools_des())
+
+# tools des in list of dict
+TOOLS_DES = [json.loads(o.model_dump_json()) for o in TOOLS]
+
+# the header of function call
+FUNC_CALL_HEADER = r"""You have access to functions. If you decide to invoke any of the function(s),
+ you MUST put it in the format of
+[func_name1(params_name1=params_value1, params_name2=params_value2...), func_name2(params)] 
+
+You SHOULD NOT include any other text in the response if you call a function
+[
+  {
+    "name": "get_product_name_by_PID",
+    "description": "Finds the name of a product by its Product ID",
+    "parameters": {
+      "type": "object",
+      "properties": {
+        "PID": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "PID"
+      ]
+    }
+  }
+]
+"""
+
+
+def call_gemma():
+    from mlx_lm import generate, load
+
+    model, tokenizer = load("mlx-community/gemma-3-4b-it-4bit-DWQ")
+
+    prompt = "hello"
+
+    if tokenizer.chat_template is not None:
+        messages = [{"role": "user", "content": prompt}]
+        prompt = tokenizer.apply_chat_template(messages, add_generation_prompt=True)
+
+        response = generate(model, tokenizer, prompt=prompt, verbose=True)
+        print("{}", response)
+
+
+async def main():
+    # Connect via SSE
+    pass
