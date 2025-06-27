3e99495d1712249492e83f4a2b421f030d823d22
ccQpein <ccqpein@protonmail.com>
2025-06-27 22:27:05 +0800

    a bit

--- rusty/dioxus-demos/tutorial/hot_dog/src/guide_state.rs
+++ rusty/dioxus-demos/tutorial/hot_dog/src/guide_state.rs
@@ -68,18 +68,47 @@ fn Title() -> Element {
 //     }
 // }
 
+#[derive(serde::Deserialize)]
+struct DogApi {
+    message: String,
+}
+
 #[component]
 pub fn DogView() -> Element {
-    let skip = move |evt| {};
-    let save = move |evt| {};
-    let img_src = use_hook(|| "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg");
+    // let mut img_src = use_signal(|| "".to_string());
+
+    // let fetch_new = move |_| async move {
+    //     let response = reqwest::get("https://dog.ceo/api/breeds/image/random")
+    //         .await
+    //         .unwrap()
+    //         .json::<DogApi>()
+    //         .await
+    //         .unwrap();
+
+    //     img_src.set(response.message);
+    // };
+
+    // let skip = move |evt| {};
+    // let save = move |evt| {};
+    // let img_src = use_hook(|| "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg");
+
+    let mut img_src = use_resource(|| async move {
+        reqwest::get("https://dog.ceo/api/breeds/image/random")
+            .await
+            .unwrap()
+            .json::<DogApi>()
+            .await
+            .unwrap()
+            .message
+    });
     rsx! {
         div { id: "dogview",
-              img { src: "{img_src}" }
+              //img { src: "{img_src}" }
+              img { src: img_src.cloned().unwrap_or_default() }
         }
         div { id: "buttons",
-              button { onclick: skip, id: "skip",  "skip" }
-              button { onclick: save, id: "save",  "save!" }
+              button { onclick:  move |_| img_src.restart(), id: "skip",  "skip" }
+              button { onclick:  move |_| img_src.restart(), id: "save",  "save!" }
         }
     }
 }
