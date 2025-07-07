3e7586ee3d9575572643f58746619769933d89b6
ccQpein <ccqpein@protonmail.com>
2025-07-07 00:15:22 -0400

    very simple

--- web/blog0/src/home.rs
+++ web/blog0/src/home.rs
@@ -1,8 +1,23 @@
 use dioxus::prelude::*;
 
 #[component]
-fn Home() -> Element {
+pub fn Home() -> Element {
+    let mut all_posts = use_signal(|| {
+        //:= need to timer read the all files inside the posts
+        vec!["aa".to_string(), "bb".to_string(), "cc".to_string()]
+    });
+
+    // read the posts from posts folder
+
     rsx! {
-        for p in all_posts {}
+        for p in all_posts() {
+            Blog{content: p},
+            br{}
+        }
     }
+}
+
+#[component]
+fn Blog(content: String) -> Element {
+    rsx! {"{content}"}
 }
