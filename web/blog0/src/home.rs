68fb5664cf023cafaa661af596c80e6b89042cd8
ccQpein <ccqpein@protonmail.com>
2025-07-10 20:47:29 -0400

    lets do some rust instead

--- web/blog0/src/home.rs
+++ web/blog0/src/home.rs
@@ -1,3 +1,5 @@
+use std::{cell::RefCell, rc::Rc};
+
 use chrono::Local;
 use dioxus::prelude::*;
 
@@ -6,7 +8,7 @@ struct BlogProps {
     title: String,
     content: String,
     //createAt: String,
-    this_post: Signal<Option<String>>,
+    this_post: Rc<RefCell<Signal<Option<String>>>>,
 }
 
 #[component]
@@ -15,17 +17,17 @@ fn Blog(props: BlogProps) -> Element {
         div {
             class: "bg-white p-6 rounded-lg shadow-md cursor-pointer w-80",
 
-            onclick: move |_| props.this_post.set(Some(props.title.clone())),
+            onclick: move |_| props.this_post.borrow_mut().set(Some(props.title.clone())),
 
             h2 {
                 class: "text-xl font-bold mb-2 text-center text-gray-800",
                 "{props.title}",
             }
 
-            if props.this_post().is_some() && props.this_post().clone().unwrap() == title {
+            if (props.this_post.borrow())().is_some() && (props.this_post.borrow())().clone().unwrap() == props.title {
                 div {
                     class: "mt-4 pt-4 border-t border-gray-200 text-gray-600 text-sm text-center",
-                    "{content}",
+                    "{props.content}",
                     br{}
                     "next",
                 }
@@ -34,17 +36,36 @@ fn Blog(props: BlogProps) -> Element {
     }
 }
 
-// #[derive(Debug, Clone)]
-// struct BlogList {
-//     allBlogs: Vec<Blog>,
-// }
+#[derive(Props, PartialEq, Debug, Clone)]
+struct BlogViewProps {
+    all_posts: Vec<String>,
+    this_post: Rc<RefCell<Signal<Option<String>>>>,
+}
+
+#[component]
+fn BlogView(props: BlogViewProps) -> Element {
+    if (props.this_post).borrow()().is_none() {
+        return rsx! {
+            for p in props.all_posts {
+                Blog{title: p.clone(), content: p, this_post:Rc::clone(&props.this_post)} //:= should be the title and click open the whole posts
+                br{}
+            }
+        };
+    } else {
+        for p in props.all_posts {
+            if p == (props.this_post).borrow()().unwrap_or("".to_string()) {
+                return rsx! {
+                    Blog{title: p.clone(), content: p, this_post: Rc::clone(&props.this_post)}
+                };
+            }
+        }
+        rsx!()
+    }
+}
 
 #[component]
 pub fn Home() -> Element {
-    let mut all_posts = use_signal(|| {
-        //:= need to timer read the all files inside the posts
-        vec!["aa".to_string(), "bb".to_string(), "cc".to_string()]
-    });
+    let mut all_posts = use_signal(|| vec!["aa".to_string(), "bb".to_string(), "cc".to_string()]);
 
     let this_post = use_signal(|| Option::<String>::None);
     use_effect(move || match this_post() {
@@ -56,55 +77,7 @@ pub fn Home() -> Element {
         }
     });
 
-    // let post = use_effect(|| {
-    //     if this_post().is_none() {
-    //         rsx! {
-    //             for p in all_posts() {
-
-    //                 Blog{title: p.clone(), content: p, this_post:this_post.clone()} //:= should be the title and click open the whole posts
-    //                 br{}}
-    //         }
-    //     }
-    //     // else {
-    //     //     for p in all_posts() {
-    //     //         if &p == this_post().as_ref().unwrap() {
-    //     //             return rsx! {
-    //     //                 Blog{title: p.clone(), content: p, this_post:this_post.clone()},
-    //     //                 br {},
-    //     //                 "haha"
-    //     //             };
-    //     //         }
-    //     //     }
-    //     // }
-    // });
-
     rsx! {
-        // for p in all_posts() {
-        //     Blog{title: p.clone(), content: p, this_post:this_post}
-        //     br{}
-        // }
-
-        BlogList{all_posts: all_posts(), this_post}
-    }
-}
-
-#[component]
-fn BlogList(all_posts: Vec<String>, this_post: Signal<Option<String>>) -> Element {
-    if this_post().is_none() {
-        return rsx! {
-            for p in all_posts {
-                Blog{title: p.clone(), content: p, this_post:this_post} //:= should be the title and click open the whole posts
-                br{}
-            }
-        };
-    } else {
-        for p in all_posts {
-            if p == this_post().clone().unwrap() {
-                return rsx! {
-                    Blog{title: p.clone(), content: p, this_post:this_post}
-                };
-            }
-        }
-        rsx!()
+        BlogView{all_posts: all_posts(), this_post: Rc::new(RefCell::new(this_post))}
     }
 }
