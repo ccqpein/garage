c3cc2056ed5415e7da883b3b0906c891e188a2d6
ccQpein <ccqpein@protonmail.com>
2025-10-02 22:06:56 -0400

    a bit

--- rusty/ratzilla-demo/snake-game/src/snake.rs
+++ rusty/ratzilla-demo/snake-game/src/snake.rs
@@ -1,13 +1,34 @@
+//trait Direction {}
+
 /// maybe reuse it in future some time. Make it a bit abstract
 trait Snake<'s, Coord: 's> {
+    /// get the body
     fn body(&'s self) -> impl Iterator<Item = &'s Coord>;
+
+    /// this snake can grow
+    /// grow the one pixel in the front of snake
     fn grow(&'s mut self);
-    fn one_step(&'s mut self);
-    fn head(&'s self) -> Coord;
+
+    /// this snake move one step
+    /// if the head eat the food, it should grow, or keep moving
+    fn one_step(&'s mut self, food: &Coord);
+
+    // /// return the head
+    // fn head(&'s self) -> Coord;
+
+    //fn direction(&'s self) -> impl Direction;
+}
+
+enum Dir {
+    Up,
+    Down,
+    Left,
+    Right,
 }
 
 struct SnakeWidget {
     body: Vec<(u32, u32)>,
+    dir: Dir,
 }
 
 impl<'s> Snake<'s, (u32, u32)> for SnakeWidget {
@@ -19,11 +40,13 @@ impl<'s> Snake<'s, (u32, u32)> for SnakeWidget {
         todo!()
     }
 
-    fn one_step(&'s mut self) {
+    fn one_step(&'s mut self, food: &(u32, u32)) {
         todo!()
     }
 
-    fn head(&'s self) -> (u32, u32) {
-        todo!()
-    }
+    // fn head(&'s self) -> (u32, u32) {
+    //     todo!()
+    // }
+
+    //fn direction(&'s self) -> impl Direction {}
 }
