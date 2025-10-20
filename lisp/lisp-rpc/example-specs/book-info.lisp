e4ae665ecf8d1791a9ce206e1c5bddcb74a7bb1d
ccQpein <ccqpein@protonmail.com>
2025-10-20 00:42:10 -0400

    more basic work

--- lisp/lisp-rpc/example-specs/book-info.lisp
+++ lisp/lisp-rpc/example-specs/book-info.lisp
@@ -1,4 +1,16 @@
 ;;; define the spec of the api
 ;;; this should be optional
 
-(book )
+(def-rpc-package "demo")
+
+(def-msg language-perfer :lang 'string)
+
+(def-msg book-info
+  :lang 'language-perfer
+  :title 'string
+  :version 'string
+  :id 'string)
+
+(def-rpc get-book
+    '(:title 'string :vesion 'string :lang 'language-perfer)
+  'book-info)
