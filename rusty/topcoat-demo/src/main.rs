59e5d3407be82351aaadeee1abf3144963503ee1
ccQpein <ccqpein@protonmail.com>
2026-08-13 12:54:14 -0400

    a bit

--- rusty/topcoat-demo/src/main.rs
+++ rusty/topcoat-demo/src/main.rs
@@ -1,6 +1,10 @@
+use std::error::Error;
+
 use topcoat::{
     Result,
+    context::Cx,
     router::{Router, RouterBuilderDiscoverExt, page},
+    runtime::{Event, shard},
     view::{component, view},
 };
 
@@ -27,3 +31,42 @@ async fn home() -> Result {
 async fn hello(name: &str) -> Result {
     view! { <h1>"Hello, " (name) "!"</h1> }
 }
+
+// #[component]
+// async fn search() -> Result {
+//     view! {
+//         signal query = String::new();
+
+//         <input @input=$(|e: Event| query.set(e.target.value))>
+
+//         // Updates as the user types.
+//         search_results(query: $(query.get()))
+//     }
+// }
+
+// #[shard]
+// async fn search_results(cx: &Cx, query: String) -> Result {
+//     view! {
+//         <ul>
+//             // Your own server-side code, like a database query:
+//             for product in search_products(cx, &query).await? {
+//                 <li>(product.name)</li>
+//             }
+//         </ul>
+//     }
+// }
+
+// struct Product {
+//     name: String,
+// }
+
+// async fn search_products(cx: &Cx, query: String) -> Result {
+//     vec![
+//         Product {
+//             name: "a".to_string(),
+//         },
+//         Product {
+//             name: "b".to_string(),
+//         },
+//     ]
+// }
